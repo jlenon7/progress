fac5fa46318cedc9e2ebb0a71746fd8ec1bebb10
João Lenon <lenonsec7@gmail.com>
2020-04-13 17:41:25 -0300

    feat: Relacionamento Category e Product

--- ecommerce-realtime/database/migrations/1586809589909_product_schema.js
+++ ecommerce-realtime/database/migrations/1586809589909_product_schema.js
@@ -36,9 +36,28 @@ class ProductSchema extends Schema {
         .inTable('products')
         .onDelete('CASCADE')
     })
+
+    this.create('category_product', table => {
+      table.increments()
+      table.integer('product_id').unsigned()
+      table.integer('category_id').unsigned()
+
+      table
+        .foreign('product_id')
+        .references('id')
+        .inTable('products')
+        .onDelete('CASCADE')
+
+      table
+        .foreign('category_id')
+        .references('id')
+        .inTable('categories')
+        .onDelete('CASCADE')
+    })
   }
 
   down() {
+    this.drop('category_product')
     this.drop('image_product')
     this.drop('products')
   }
