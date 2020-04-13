c2d8e42b20303540bc865b8edbce46e53447efe6
João Lenon <lenonsec7@gmail.com>
2020-04-13 17:21:28 -0300

    feat: Relacionamento User e Images

--- ecommerce-realtime/database/migrations/1503250034279_user.js
+++ ecommerce-realtime/database/migrations/1503250034279_user.js
@@ -4,17 +4,19 @@
 const Schema = use('Schema')
 
 class UserSchema extends Schema {
-  up () {
-    this.create('users', (table) => {
+  up() {
+    this.create('users', table => {
       table.increments()
-      table.string('username', 80).notNullable().unique()
+      table.string('name', 80)
+      table.string('surname', 200)
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
+      table.integer('image_id').unsigned()
       table.timestamps()
     })
   }
 
-  down () {
+  down() {
     this.drop('users')
   }
 }
