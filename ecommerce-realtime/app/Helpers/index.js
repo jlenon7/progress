b9005b4334d1d496d393424c1dfa1d1665fb0582
João Lenon <lenonsec7@gmail.com>
2020-04-14 20:51:48 -0300

    Helper str_random

---
+++ ecommerce-realtime/app/Helpers/index.js
@@ -0,0 +1,32 @@
+'use script'
+
+const crypto = use('crypto')
+const Helpers = use('Helpers')
+
+/**
+ * Gerar strings randomicas
+ *
+ * @param {int} length - O tamanho da string a ser gerada
+ * @return {string} - string randomica do tamanho do lenght
+ */
+
+const str_random = async (length = 40) => {
+  let string = ''
+  let len = string.length
+
+  if (len < length) {
+    let size = length - len
+    let bytes = await crypto.randomBytes(size)
+    let buffer = new Buffer(bytes)
+    string += buffer
+      .toString('base64')
+      .replace(/[ˆa-zA-Z0-9]/g, '')
+      .substr(0, size)
+  }
+
+  return string
+}
+
+module.exports = {
+  str_random,
+}
