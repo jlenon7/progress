776b76052d864d51d4a16aa158c15bfa18fdd979
João Lenon <lenonsec7@gmail.com>
2020-04-14 19:26:18 -0300

    Index

--- ecommerce-realtime/app/Controllers/Http/Admin/UserController.js
+++ ecommerce-realtime/app/Controllers/Http/Admin/UserController.js
@@ -3,6 +3,7 @@
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
+const User = use('App/Models/User')
 
 class UserController {
   /**
@@ -12,9 +13,20 @@ class UserController {
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
-   * @param {View} ctx.view
+   * @param {object} ctx.pagination
    */
-  async index ({ request, response, view }) {
+  async index({ request, response, pagination }) {
+    const { name } = request.input('name')
+    const query = User.query()
+
+    if (name) {
+      query.where('name', 'LIKE', `%${name}%`)
+      query.orWhere('surname', 'LIKE', `%${name}%`)
+      query.orWhere('email', 'LIKE', `%${name}%`)
+    }
+
+    const users = await query.paginate(pagination.page, pagination.limit)
+    return response.json(users)
   }
 
   /**
@@ -25,8 +37,7 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
-  }
+  async store({ request, response }) {}
 
   /**
    * Display a single user.
@@ -37,8 +48,7 @@ class UserController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
+  async show({ params, request, response, view }) {}
 
   /**
    * Update user details.
@@ -48,8 +58,7 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
-  }
+  async update({ params, request, response }) {}
 
   /**
    * Delete a user with id.
@@ -59,8 +68,7 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
-  }
+  async destroy({ params, request, response }) {}
 }
 
 module.exports = UserController
