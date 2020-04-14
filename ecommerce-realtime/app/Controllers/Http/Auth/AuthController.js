67e3a4ae39b6135d879426e9795a8a3263c3c3aa
João Lenon <lenonsec7@gmail.com>
2020-04-14 17:44:02 -0300

    Return in json

--- ecommerce-realtime/app/Controllers/Http/Auth/AuthController.js
+++ ecommerce-realtime/app/Controllers/Http/Auth/AuthController.js
@@ -18,12 +18,12 @@ class AuthController {
 
       await trx.commit()
 
-      return response.status(201).send({ data: user })
-    } catch(error) {
+      return response.status(201).json({ data: user })
+    } catch (error) {
       await trx.rollback()
 
-      return response.status(400).send({
-        message: 'Erro ao realizar cadastro'
+      return response.status(400).json({
+        message: 'Erro ao realizar cadastro',
       })
     }
   }
@@ -33,31 +33,33 @@ class AuthController {
 
     let data = await auth.withRefreshToken().attempt(email, password)
 
-    return response.send({ data })
+    return response.json({ data })
   }
 
   async refresh({ request, response, auth }) {
     var refresh_token = request.input('refresh_token')
 
-    if(!refresh_token) {
+    if (!refresh_token) {
       refresh_token = request.header('refresh_token')
     }
 
-    const user = await auth.newRefreshToken().generateForRefreshToken(refresh_token)
+    const user = await auth
+      .newRefreshToken()
+      .generateForRefreshToken(refresh_token)
 
-    return response.send({ data: user })
+    return response.json({ data: user })
   }
 
   async logout({ request, response, auth }) {
     let refresh_token = request.input('refresh_token')
 
-    if(!refresh_token) {
+    if (!refresh_token) {
       refresh_token = request.header('refresh_token')
     }
 
     await auth.authenticator('jwt').revokeTokens([refresh_token], true)
 
-    return response.status(204).send({})
+    return response.status(204).json({})
   }
 
   async forgot({ request, response }) {
