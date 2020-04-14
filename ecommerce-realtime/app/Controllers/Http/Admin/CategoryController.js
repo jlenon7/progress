16983abc71ec6fda37af2d4d3a36872eee018c19
João Lenon <lenonsec7@gmail.com>
2020-04-14 17:43:32 -0300

    Index with pagination

--- ecommerce-realtime/app/Controllers/Http/Admin/CategoryController.js
+++ ecommerce-realtime/app/Controllers/Http/Admin/CategoryController.js
@@ -3,6 +3,7 @@
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
+const Category = use('App/Models/Category')
 
 class CategoryController {
   /**
@@ -12,9 +13,20 @@ class CategoryController {
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
-   * @param {View} ctx.view
+   * @param {object} ctx.pagination
    */
-  async index ({ request, response, view }) {
+  async index({ request, response, pagination }) {
+    const title = request.input('title')
+
+    const query = Category.query()
+
+    if (title) {
+      query.where('title', 'LIKE', `%${title}%`)
+    }
+
+    const categories = await query.paginate(pagination.page, pagination.limit)
+
+    return response.json(categories)
   }
 
   /**
@@ -25,8 +37,7 @@ class CategoryController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
-  }
+  async store({ request, response }) {}
 
   /**
    * Display a single category.
@@ -37,8 +48,7 @@ class CategoryController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
+  async show({ params, request, response, view }) {}
 
   /**
    * Update category details.
@@ -48,8 +58,7 @@ class CategoryController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
-  }
+  async update({ params, request, response }) {}
 
   /**
    * Delete a category with id.
@@ -59,8 +68,7 @@ class CategoryController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
-  }
+  async destroy({ params, request, response }) {}
 }
 
 module.exports = CategoryController
