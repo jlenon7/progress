40bd3ce2ffaa7c07bad313876d9b723b31096e98
João Lenon <lenonsec7@gmail.com>
2020-04-14 19:07:18 -0300

    Destroy

--- ecommerce-realtime/app/Controllers/Http/Admin/ProductController.js
+++ ecommerce-realtime/app/Controllers/Http/Admin/ProductController.js
@@ -64,7 +64,11 @@ class ProductController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show({ params, request, response, view }) {}
+  async show({ params: { id }, request, response }) {
+    const product = await Product.findOrFail(id)
+
+    return response.json(product)
+  }
 
   /**
    * Update product details.
@@ -99,7 +103,19 @@ class ProductController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy({ params, request, response }) {}
+  async destroy({ params: { id }, request, response }) {
+    const product = await Product.findOrFail(id)
+
+    try {
+      await product.delete()
+
+      return response.status(204).json()
+    } catch (error) {
+      return response.status(500).json({
+        message: 'Não foi possível deletar este produto',
+      })
+    }
+  }
 }
 
 module.exports = ProductController
