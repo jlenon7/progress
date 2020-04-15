24f1613ffd2d70d2f4eeb246e766af2b35cbc1ad
João Lenon <lenonsec7@gmail.com>
2020-04-15 18:00:28 -0300

    Messages on Validator

--- ecommerce-realtime/app/Validators/Auth/Register.js
+++ ecommerce-realtime/app/Validators/Auth/Register.js
@@ -10,6 +10,18 @@ class AuthRegister {
       password: 'required|confirmed',
     }
   }
+
+  get messages() {
+    return {
+      'name.required': 'O nome é obrigatório',
+      'surname.required': 'O sobrenome é obrigatório',
+      'email.required': 'O e-mail é obrigatório',
+      'email.email': 'E-mail inválido',
+      'email.unique': 'Este e-mail já existe',
+      'password.required': 'A senha é obrigatória',
+      'password.confirmed': 'As senhas não são iguais',
+    }
+  }
 }
 
 module.exports = AuthRegister
