b46d431a7d61f2af770929f616468f0140f05877
João Lenon <lenonsec7@gmail.com>
2020-04-15 16:17:22 -0300

    Login Validators

---
+++ ecommerce-realtime/app/Validators/Auth/Login.js
@@ -0,0 +1,13 @@
+'use strict'
+
+class Login {
+  get rules() {
+    return {
+      // validation rules
+      email: 'required|email',
+      password: 'required',
+    }
+  }
+}
+
+module.exports = Login
