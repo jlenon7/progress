03ad2572e603b833a76f9ae2a2a102290af6b116
João Lenon <lenonsec7@gmail.com>
2020-04-14 22:18:26 -0300

    manage_single_upload

--- ecommerce-realtime/app/Helpers/index.js
+++ ecommerce-realtime/app/Helpers/index.js
@@ -27,6 +27,29 @@ const str_random = async (length = 40) => {
   return string
 }
 
+/**
+ * Gerenciar upload de um único arquivo
+ *
+ * @param {FileJar} file - Arquivo
+ * @param {string} path - o caminho para onde o arquivo deve ser movido
+ * @return {object<FileJar>}
+ */
+
+const manage_single_upload = async (file, path = null) => {
+  path = path ? path : Helpers.publicPath('uploads')
+  // Gera nome aleatório
+  const random_name = await str_random(10)
+  let filename = `${new Date().getTime()}-${random_name}.${file.subtype}`
+
+  // Renomear arquivo e mover ele para o path
+  await file.move(path, {
+    name: filename,
+  })
+
+  return file
+}
+
 module.exports = {
   str_random,
+  manage_single_upload,
 }
