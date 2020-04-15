aae23bfef2d83ea14d238d4a58cdc475f2a23a56
João Lenon <lenonsec7@gmail.com>
2020-04-15 00:34:28 -0300

    feat: Migrations

---
+++ database/migrations/1586810934307_coupon_order_schema.js
@@ -0,0 +1,34 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CouponOrderSchema extends Schema {
+  up() {
+    this.create('coupon_order', table => {
+      table.increments()
+      table.integer('coupon_id').unsigned()
+      table.integer('order_id').unsigned()
+      table.decimal('discount', 12, 2).defaultTo(0.0)
+      table.timestamps()
+
+      table
+        .foreign('coupon_id')
+        .references('id')
+        .inTable('coupons')
+        .onDelete('CASCADE')
+
+      table
+        .foreign('order_id')
+        .references('id')
+        .inTable('orders')
+        .onDelete('CASCADE')
+    })
+  }
+
+  down() {
+    this.drop('coupon_order')
+  }
+}
+
+module.exports = CouponOrderSchema
