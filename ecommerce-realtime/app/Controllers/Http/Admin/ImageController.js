499aead642b8189313cef9e3239ff75513312e29
João Lenon <lenonsec7@gmail.com>
2020-04-14 22:35:13 -0300

    Show

--- ecommerce-realtime/app/Controllers/Http/Admin/ImageController.js
+++ ecommerce-realtime/app/Controllers/Http/Admin/ImageController.js
@@ -97,7 +97,11 @@ class ImageController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show({ params, request, response, view }) {}
+  async show({ params: { id }, request, response, view }) {
+    const image = await Image.findOrFail()
+
+    return response.json(image)
+  }
 
   /**
    * Update image details.
