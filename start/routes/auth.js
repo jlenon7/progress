e277e19c0301c240d597ccd9f6092d86e0811ea5
João Lenon <lenonsec7@gmail.com>
2020-04-15 00:35:26 -0300

    feat: Auth Routes

---
+++ start/routes/auth.js
@@ -0,0 +1,18 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+// Auth Routes
+
+Route.group(() => {
+  Route.post('/register', 'AuthController.register').as('auth.register')
+  Route.post('/login', 'AuthController.login').as('auth.login')
+  Route.post('/refresh', 'AuthController.refresh').as('auth.refresh')
+  Route.post('/logout', 'AuthController.logout').as('auth.logout')
+
+  // Password restore
+  Route.post('/reset-password', 'AuthController.forgot').as('auth.forgot')
+  Route.get('/reset-password', 'AuthController.remember').as('auth.remember')
+  Route.put('/reset-password', 'AuthController.reset').as('auth.reset')
+}).prefix('/api/v1/auth').namespace('Auth')
