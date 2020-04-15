eec95269fec19a9690aa4c3877065ff23f0aeff5
João Lenon <lenonsec7@gmail.com>
2020-04-15 14:48:37 -0300

    Update

--- ecommerce-realtime/app/Controllers/Http/Admin/OrderController.js
+++ ecommerce-realtime/app/Controllers/Http/Admin/OrderController.js
@@ -88,7 +88,30 @@ class OrderController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update({ params, request, response }) {}
+  async update({ params: { id }, request, response }) {
+    const order = await Order.findOrFail(id)
+
+    const trx = await Database.beginTransaction()
+    try {
+      const { user_id, items, status } = request.all()
+
+      order.merge({ user_id, status })
+      const service = new Service(order, trx)
+
+      await service.updateItems(items)
+      await order.save(trx)
+
+      await trx.commit()
+
+      return response.json(order)
+    } catch (error) {
+      await trx.rollback()
+
+      return response.status(400).json({
+        message: 'Não foi possível atualizar o pedido no momento',
+      })
+    }
+  }
 
   /**
    * Delete a order with id.
