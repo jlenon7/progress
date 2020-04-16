0b6616f2c24e1dea3d6c455fabafbd483bb247ee
João Lenon <lenonsec7@gmail.com>
2020-04-16 19:00:47 -0300

    Bug fix

--- app/Exceptions/Handler.js
+++ app/Exceptions/Handler.js
@@ -21,8 +21,12 @@ class ExceptionHandler extends BaseExceptionHandler {
    *
    * @return {void}
    */
-  async handle (error, { request, response }) {
-    response.status(error.status).send(error.message)
+  async handle(error, { request, response }) {
+    if (error.name === 'ValidationException') {
+      response.status(error.status).json({
+        message: error.messages,
+      })
+    }
   }
 
   /**
@@ -35,13 +39,13 @@ class ExceptionHandler extends BaseExceptionHandler {
    *
    * @return {void}
    */
-  async report (error, { request }) {
-    if(error.status >= 500){
+  async report(error, { request }) {
+    if (error.status >= 500) {
       Logger.error(error.message, {
         stack: error.stack,
         message: error.message,
-        status: error.status, 
-        name: error.name
+        status: error.status,
+        name: error.name,
       })
     }
   }
