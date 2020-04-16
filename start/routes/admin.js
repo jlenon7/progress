aaa60d5a95889e7dc708fb5409da9c7ff5db7148
João Lenon <lenonsec7@gmail.com>
2020-04-16 19:00:17 -0300

    New Routes and Prettier

--- start/routes/admin.js
+++ start/routes/admin.js
@@ -5,7 +5,14 @@ const Route = use('Route')
 
 Route.group(() => {
   // Category Resource
-  Route.resource('categories', 'CategoryController').apiOnly()
+  Route.resource('categories', 'CategoryController')
+    .apiOnly()
+    .validator(
+      new Map([
+        [['categories.store'], ['Admin/Category/StoreCategory']],
+        [['categories.update'], ['Admin/Category/StoreCategory']],
+      ])
+    )
 
   // Coupon Resource
   Route.resource('coupons', 'CouponController').apiOnly()
@@ -13,12 +20,27 @@ Route.group(() => {
   // Image Resource
   Route.resource('images', 'ImageController').apiOnly()
 
+  Route.post('orders/:id/discount', 'OrderController.applyDiscount')
+  Route.delete('orders/:id/discount', 'OrderController.revemoDiscount')
+
   // Order Resource
-  Route.resource('orders', 'OrderController').apiOnly()
+  Route.resource('orders', 'OrderController')
+    .apiOnly()
+    .validator(new Map([[['orders.store'], ['Admin/Category/StoreOrder']]]))
 
   // Product Resource
   Route.resource('products', 'ProductController').apiOnly()
 
   // User Resource
-  Route.resource('users', 'UserController').apiOnly()
-}).prefix('/api/v1/admin').namespace('Admin')
+  Route.resource('users', 'UserController')
+    .apiOnly()
+    .validator(
+      new Map([
+        [['users.store'], ['Admin/User/StoreUser']],
+        [['users.update'], ['Admin/User/StoreUser']],
+      ])
+    )
+})
+  .prefix('/api/v1/admin')
+  .namespace('Admin')
+  .middleware(['auth', 'is:(admin || manager)'])
