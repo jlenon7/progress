7c80121abeb53e0bcd193af0aa80dcff9bc98f02
João Lenon <lenonsec7@gmail.com>
2020-05-03 13:14:33 -0300

    feat: ReactJS

--- LVL 03/gobarber/src/components/ToastContainer/index.tsx
+++ LVL 03/gobarber/src/components/ToastContainer/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { Container } from './styles'
+import { useTransition } from 'react-spring'
 
 import Toast from './Toast'
 
@@ -11,10 +12,20 @@ interface ToastContainerProps {
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+  const messagesWithTransitions = useTransition(
+    messages,
+    message => message.id,
+    {
+      from: { right: '-120%', opacity: 0, transform: 'rotateX(0deg)' },
+      enter: { right: '0%', opacity: 1, transform: 'rotateX(360deg)' },
+      leave: { right: '-120%', opacity: 0, transform: 'rotateX(0deg)' },
+    },
+  )
+
   return (
     <Container>
-      {messages.map(message => (
-        <Toast key={message.id} message={message} />
+      {messagesWithTransitions.map(({ item, key, props }) => (
+        <Toast key={key} style={props} message={item} />
       ))}
     </Container>
   )
