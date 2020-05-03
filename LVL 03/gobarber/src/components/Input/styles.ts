1fb38530da53f082058964cd2977d03f2f615ebb
João Lenon <lenonsec7@gmail.com>
2020-05-03 12:39:34 -0300

    feat: ReactJS

--- LVL 03/gobarber/src/components/Input/styles.ts
+++ LVL 03/gobarber/src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components'
 
+import Tooltip from '../Tooltip'
+
 interface ContainerProps {
   isFocused: boolean
   isFilled: boolean
+  isErrored: boolean
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -22,6 +25,12 @@ export const Container = styled.div<ContainerProps>`
   & + div {
     margin-top: 8px;
   }
+
+  ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
 
   ${props =>
     props.isFocused &&
@@ -49,5 +58,23 @@ export const Container = styled.div<ContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `
