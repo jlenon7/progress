338aa68894eaa9828b07b7148f68863ffad84ce0
João Lenon <lenonsec7@gmail.com>
2020-05-03 12:38:50 -0300

    feat: ReactJS

--- LVL 02/Iniciando backend do app/src/server.ts
+++ LVL 02/Iniciando backend do app/src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata'
 
 import express, { Request, Response, NextFunction } from 'express'
+import cors from 'cors'
 import 'express-async-errors'
 import routes from './Routes'
 import uploadConfig from './Config/upload'
@@ -11,6 +12,7 @@ import './Database'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/files', express.static(uploadConfig.directory))
 app.use(routes)
