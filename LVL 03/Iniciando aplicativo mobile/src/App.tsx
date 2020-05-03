2b9cb07e46439b278cc053f195218522228e98ce
João Lenon <lenonsec7@gmail.com>
2020-05-03 20:55:44 -0300

    feat: React Native

--- LVL 03/Iniciando aplicativo mobile/src/App.tsx
+++ LVL 03/Iniciando aplicativo mobile/src/App.tsx
@@ -1,6 +1,18 @@
+import 'react-native-gesture-handler'
 import React from 'react'
-import { View } from 'react-native'
+import { View, StatusBar } from 'react-native'
 
-const App: React.FC = () => <View/>
+import { NavigationContainer } from '@react-navigation/native'
+
+import Routes from './routes'
+
+const App: React.FC = () => (
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor="312e38"/>
+    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+      <Routes/>
+    </View>
+  </NavigationContainer>
+)
 
 export default App
