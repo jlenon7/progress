4d8497c7213c4e3756b67bed7c628d69c44f30bd
João Lenon <lenonsec7@gmail.com>
2020-05-04 17:00:55 -0300

    feat: Ruge project update

--- ecommerce-realtime/app/Helpers/index.js
+++ ecommerce-realtime/app/Helpers/index.js
@@ -1,15 +1,14 @@
-'use script'
+'use stric'
 
 const crypto = use('crypto')
 const Helpers = use('Helpers')
 
 /**
- * Gerar strings randomicas
+ * Generate random string
  *
- * @param {int} length - O tamanho da string a ser gerada
- * @return {string} - string randomica do tamanho do lenght
+ * @param { int } length - O  tamanho da string que você quer gerar
+ * @return { string } uma string randomica do tamaho de length
  */
-
 const str_random = async (length = 40) => {
   let string = ''
   let len = string.length
@@ -23,54 +22,52 @@ const str_random = async (length = 40) => {
       .replace(/[^a-zA-Z0-9]/g, '')
       .substr(0, size)
   }
-
   return string
 }
 
 /**
- * Gerenciar upload de um único arquivo
- *
- * @param {FileJar} file - Arquivo
- * @param {string} path - o caminho para onde o arquivo deve ser movido
- * @return {object<FileJar>}
+ * Move um único arquivo para o caminho especificado, se nenhum for especificado
+ * então 'public/uploads' será utilizado.
+ * @param { FileJar } file o arquivo a ser gerenciado
+ * @param { string } path o caminho para onde o arquivo deve ser movido
+ * @return { Object<FileJar> }
  */
-
 const manage_single_upload = async (file, path = null) => {
   path = path ? path : Helpers.publicPath('uploads')
-  // Gera nome aleatório
-  const random_name = await str_random(10)
+  // gera um nome aleatório
+  const random_name = await str_random(30)
   let filename = `${new Date().getTime()}-${random_name}.${file.subtype}`
 
-  // Renomear arquivo e mover ele para o path
+  // renomeia o arquivo e move ele para o path
   await file.move(path, {
-    name: filename,
+    name: filename
   })
 
   return file
 }
 
 /**
- * Gerenciar upload de um vários arquivos
- *
- * @param {FileJar} fileJar - Arquivos
- * @param {string} path - o caminho para onde o arquivo deve ser movido
- * @return {object}
+ * Move um múltiplos arquivos para o caminho especificado, se nenhum for especificado
+ * então 'public/uploads' será utilizado.
+ * @param { FileJar } fileJar
+ * @param { string } path
+ * @return { Object }
  */
-
 const manage_multiple_uploads = async (fileJar, path = null) => {
   path = path ? path : Helpers.publicPath('uploads')
-  let successes = []
-  errors = []
+  let successes = [],
+    errors = []
 
   await Promise.all(
     fileJar.files.map(async file => {
-      let random_name = await str_random(10)
+      let random_name = await str_random(30)
       let filename = `${new Date().getTime()}-${random_name}.${file.subtype}`
-
+      // move o arquivo
       await file.move(path, {
-        name: filename,
+        name: filename
       })
 
+      // verificamos se moveu mesmo
       if (file.moved()) {
         successes.push(file)
       } else {
@@ -81,9 +78,8 @@ const manage_multiple_uploads = async (fileJar, path = null) => {
 
   return { successes, errors }
 }
-
 module.exports = {
   str_random,
   manage_single_upload,
-  manage_multiple_uploads,
+  manage_multiple_uploads
 }
