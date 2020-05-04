353061697566117feaa8d937cef2bbf6eff6ab79
João Lenon <lenonsec7@gmail.com>
2020-05-04 17:03:13 -0300

    feat: Ruge project update

---
+++ ecommerce-realtime/database/migrations/1552808268928_coupon_schema.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CouponSchema extends Schema {
+  up() {
+    this.create('coupons', table => {
+      table.increments()
+      table.string('code', 100).notNullable()
+      table.dateTime('valid_from')
+      table.dateTime('valid_until')
+      table.integer('discount')
+      table.integer('quantity').defaultTo(1)
+      table.enu('can_use_for', ['product', 'client', 'product_client', 'all'])
+
+      table.enu('type', ['free', 'percent', 'currency']).defaultTo('currency')
+      table.boolean('recursive').defaultTo(false)
+      table.timestamps()
+    })
+  }
+
+  down() {
+    this.drop('coupons')
+  }
+}
+
+module.exports = CouponSchema
