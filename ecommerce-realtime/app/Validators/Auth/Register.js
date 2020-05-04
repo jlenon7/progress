da95af111a796c9405c01de6707c3bf08e9ead50
João Lenon <lenonsec7@gmail.com>
2020-05-04 17:01:45 -0300

    feat: Ruge project update

--- ecommerce-realtime/app/Validators/Auth/Register.js
+++ ecommerce-realtime/app/Validators/Auth/Register.js
@@ -3,23 +3,22 @@
 class AuthRegister {
   get rules() {
     return {
-      // validation rules
       name: 'required',
       surname: 'required',
       email: 'required|email|unique:users,email',
-      password: 'required|confirmed',
+      password: 'required|confirmed'
     }
   }
 
   get messages() {
     return {
-      'name.required': 'O nome é obrigatório',
-      'surname.required': 'O sobrenome é obrigatório',
-      'email.required': 'O e-mail é obrigatório',
-      'email.email': 'E-mail inválido',
-      'email.unique': 'Este e-mail já existe',
-      'password.required': 'A senha é obrigatória',
-      'password.confirmed': 'As senhas não são iguais',
+      'name.required': 'O nome é obrigatório!',
+      'surname.required': 'O sobrenome é Obrigatório!',
+      'email.required': 'O e-mail é obrigatório!',
+      'email.email': 'E-mail inválido!',
+      'email.unique': 'Este E-mail já existe!',
+      'password.required': 'A senha é obrigatória!',
+      'password.confirmed': 'As senhas não são iguais!'
     }
   }
 }
