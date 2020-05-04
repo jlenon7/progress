6a3155d57bb2cb94c5d336c9df2f4fe3353f2018
João Lenon <lenonsec7@gmail.com>
2020-05-04 17:03:37 -0300

    feat: Ruge project update

--- ecommerce-realtime/app/Transformers/Admin/CategoryTransformer.js
+++ ecommerce-realtime/app/Transformers/Admin/CategoryTransformer.js
@@ -1,18 +1,18 @@
 'use strict'
 
-const BumblebeeTransformer = use('Bumblebee/Transformer')
+const TransformerAbstract = use('Adonis/Addons/Bumblebee/TransformerAbstract')
 const ImageTransformer = use('App/Transformers/Admin/ImageTransformer')
-
 /**
  * CategoryTransformer class
  *
  * @class CategoryTransformer
  * @constructor
  */
-class CategoryTransformer extends BumblebeeTransformer {
-  static get defaultInclude() {
+class CategoryTransformer extends TransformerAbstract {
+  defaultInclude() {
     return ['image']
   }
+
   /**
    * This method is used to transform the data.
    */
@@ -20,7 +20,7 @@ class CategoryTransformer extends BumblebeeTransformer {
     return {
       id: model.id,
       title: model.title,
-      description: model.description,
+      description: model.description
     }
   }
 
