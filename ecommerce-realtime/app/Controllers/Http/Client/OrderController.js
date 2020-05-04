a926bacfdccd1be265f65040ced5e0594a7af2e3
João Lenon <lenonsec7@gmail.com>
2020-05-04 17:01:20 -0300

    feat: Ruge project update

--- ecommerce-realtime/app/Controllers/Http/Client/OrderController.js
+++ ecommerce-realtime/app/Controllers/Http/Client/OrderController.js
@@ -4,6 +4,17 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Order = use('App/Models/Order')
+const Transformer = use('App/Transformers/Admin/OrderTransformer')
+const Database = use('Database')
+const Service = use('App/Services/Order/OrderService')
+const Ws = use('Ws')
+const Coupon = use('App/Models/Coupon')
+const Discount = use('App/Models/Discount')
+
+/**
+ * Resourceful controller for interacting with orders
+ */
 class OrderController {
   /**
    * Show a list of all orders.
@@ -14,7 +25,24 @@ class OrderController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
+  async index({ request, response, transform, pagination, auth }) {
+    // order number
+    const client = await auth.getUser()
+    const number = request.input('number')
+    const query = Order.query()
+    if (number) {
+      query.where('id', 'LIKE', `${number}`)
+    }
+
+    query.where('user_id', client.id)
+
+    const results = await query
+      .orderBy('id', 'DESC')
+      .paginate(pagination.page, pagination.limit)
+
+    const orders = await transform.paginate(results, Transformer)
+
+    return response.send(orders)
   }
 
   /**
@@ -25,7 +53,32 @@ class OrderController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store({ request, response, auth, transform }) {
+    const trx = await Database.beginTransaction()
+    try {
+      const items = request.input('items') // array
+      const client = await auth.getUser()
+      var order = await Order.create({ user_id: client.id }, trx)
+      const service = new Service(order, trx)
+      if (items.length > 0) {
+        await service.syncItems(items)
+      }
+      await trx.commit()
+      // instancia os hooks de cálculos dos subtotais
+      order = await Order.find(order.id)
+      order = await transform.include('items').item(order, Transformer)
+      // emite um broadcast no websocket
+      const topic = Ws.getChannel('notifications').topic('notifications')
+      if (topic) {
+        topic.broadcast('new:order', order)
+      }
+      return response.status(201).send(order)
+    } catch (error) {
+      await trx.rollback()
+      return response.status(400).send({
+        message: 'Não foi possível fazer seu pedido!'
+      })
+    }
   }
 
   /**
@@ -37,7 +90,14 @@ class OrderController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show({ params: { id }, response, transform, auth }) {
+    const client = await auth.getUser()
+    const result = await Order.query()
+      .where('user_id', client.id)
+      .where('id', id)
+      .firstOrFail()
+    const order = await transform.item(result, Transformer)
+    return response.send(order)
   }
 
   /**
@@ -48,9 +108,80 @@ class OrderController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update({ params: { id }, request, response, auth, transform }) {
+    const client = await auth.getUser()
+    var order = await Order.query()
+      .where('user_id', client.id)
+      .where('id', id)
+      .firstOrFail()
+
+    const trx = await Database.beginTransaction()
+    try {
+      const { items, status } = request.all()
+      order.merge({ user_id: client.id, status })
+      const service = new Service(order, trx)
+      await service.updateItems(items)
+      await order.save(trx)
+      await trx.commit()
+      order = await transform
+        .include('items,coupons,discounts')
+        .item(order, Transformer)
+      return response.send(order)
+    } catch (error) {
+      await trx.rollback()
+      return response.status(400).send({
+        message: 'Não foi possível atualizar o seu pedido!'
+      })
+    }
   }
 
+  async applyDiscount({ params: { id }, request, response, transform, auth }) {
+    const { code } = request.all()
+    const coupon = await Coupon.findByOrFail('code', code.toUpperCase())
+    const client = await auth.getUser()
+    var order = await Order.query()
+      .where('user_id', client.id)
+      .where('id', id)
+      .firstOrFail()
+
+    var discount,
+      info = {}
+    try {
+      const service = new Service(order)
+      const canAddDiscount = await service.canApplyDiscount(coupon)
+      const orderDiscounts = await order.coupons().getCount()
+
+      const canApplyToOrder =
+        orderDiscounts < 1 || (orderDiscounts >= 1 && coupon.recursive)
+      if (canAddDiscount && canApplyToOrder) {
+        discount = await Discount.findOrCreate({
+          order_id: order.id,
+          coupon_id: coupon.id
+        })
+
+        info.message = 'Cupom aplicado com sucesso!'
+        info.success = true
+      } else {
+        info.message = 'Não foi possível aplicar o cupom!'
+        info.success = false
+      }
+      order = await transform
+        .include('coupons,items,discounts')
+        .item(order, Transformer)
+      return response.send({ order, info })
+    } catch (error) {
+      return response.status(400).send({
+        message: 'Erro desconhecido!'
+      })
+    }
+  }
+
+  async removeDiscount({ request, response }) {
+    const { discount_id } = request.all()
+    const discount = await Discount.findOrFail(discount_id)
+    await discount.delete()
+    return response.status(204).send()
+  }
 }
 
 module.exports = OrderController
--- ecommerce-realtime/app/Middleware/Pagination.js
+++ ecommerce-realtime/app/Middleware/Pagination.js
@@ -10,14 +10,18 @@ class Pagination {
    * @param {Function} next
    */
   async handle(ctx, next) {
+    // call next to advance the request
     if (ctx.request.method() === 'GET') {
-      const page = parseInt(ctx.request.input('page'))
-      const limit = parseInt(ctx.request.input('limit'))
-
-      // Atribuindo valores para a propriedade pagination
+      const page = ctx.request.input('page')
+        ? parseInt(ctx.request.input('page'))
+        : 1
+      const limit = ctx.request.input('limit')
+        ? parseInt(ctx.request.input('limit'))
+        : 20
+      // atribuí os valores passados via get para a propriedade pagination do objeto ctx
       ctx.pagination = {
         page,
-        limit,
+        limit
       }
 
       const perpage = parseInt(ctx.request.input('perpage'))
