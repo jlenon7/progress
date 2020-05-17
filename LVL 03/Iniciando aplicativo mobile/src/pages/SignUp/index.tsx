3f5046f9ec7618505f37dcf83ff0ccc930dd274e
João Lenon <lenonsec7@gmail.com>
2020-05-17 12:47:31 -0300

    feat: React Native

--- LVL 03/Iniciando aplicativo mobile/src/pages/SignUp/index.tsx
+++ LVL 03/Iniciando aplicativo mobile/src/pages/SignUp/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native'
 import Icon from 'react-native-vector-icons/Feather'
 
@@ -9,8 +9,11 @@ import { Container, Title, BackToSignIn, BackToSignInText } from './styles'
 
 import logoImg from '../../assets/logo.png'
 import { useNavigation } from '@react-navigation/native'
+import { Form } from '@unform/mobile'
+import { FormHandles } from '@unform/core'
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null)
   const navigation = useNavigation()
 
   return (
@@ -24,11 +27,13 @@ const SignUp: React.FC = () => {
               <Title>Crie sua conta</Title>
             </View>
 
-            <Input name="name" icon="user" placeholder="Nome"/>
-            <Input name="email" icon="mail" placeholder="E-mail"/>
-            <Input name="password" icon="lock" placeholder="Senha"/>
+            <Form ref={formRef} onSubmit={() => {}}>
+              <Input name="name" icon="user" placeholder="Nome"/>
+              <Input name="email" icon="mail" placeholder="E-mail"/>
+              <Input name="password" icon="lock" placeholder="Senha"/>
 
-            <Button onPress={() => { console.log('Deu') }}>Entrar</Button>
+              <Button onPress={() => formRef.current?.submitForm()}>Entrar</Button>
+            </Form>
           </Container>
         </ScrollView>
         <BackToSignIn onPress={() => navigation.goBack()}>
