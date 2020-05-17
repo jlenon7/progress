8b0be7c09ea9ee774f2a56032dbe2337a2e0daaf
João Lenon <lenonsec7@gmail.com>
2020-05-17 18:45:38 -0300

    feat: React Native

--- LVL 03/Iniciando aplicativo mobile/src/components/Input/styles.ts
+++ LVL 03/Iniciando aplicativo mobile/src/components/Input/styles.ts
@@ -1,17 +1,32 @@
-import styled from 'styled-components/native'
+import styled, { css } from 'styled-components/native'
 
 import FeatherIcon from 'react-native-vector-icons/Feather'
 
-export const Container = styled.View`
+interface ContainerProps {
+  isFocused: boolean
+  isErrored: boolean
+}
+
+export const Container = styled.View<ContainerProps>`
   width: 100%;
   height: 60px;
   padding: 0 16px;
   background: #232129;
   border-radius: 10px;
   margin-bottom: 8px;
+  border-width: 2px;
+  border-color: #232129;
 
   flex-direction: row;
   align-items: center;
+
+  ${props => props.isFocused && css`
+    border-color: #ff9000;
+  `}
+
+  ${props => props.isErrored && css`
+    border-color: #c53030;
+  `}
 `
 
 export const TextInput = styled.TextInput`
