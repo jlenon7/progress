c01df8d9dc805c7441b74c0e4c616df6b25fdf72
João Lenon <lenonsec7@gmail.com>
2020-05-17 12:47:40 -0300

    feat: React Native

--- LVL 03/Iniciando aplicativo mobile/src/pages/SignIn/index.tsx
+++ LVL 03/Iniciando aplicativo mobile/src/pages/SignIn/index.tsx
@@ -1,44 +1,60 @@
-import React from 'react'
+import React, { useCallback, useRef } from 'react'
 import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native'
 import Icon from 'react-native-vector-icons/Feather'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 import { useNavigation } from '@react-navigation/native'
+import { Form } from '@unform/mobile'
+// eslint-disable-next-line no-unused-vars
+import { FormHandles } from '@unform/core'
 
 import { Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButton, CreateAccountButtonText } from './styles'
 
 import logoImg from '../../assets/logo.png'
 
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null)
   const navigation = useNavigation()
 
+  const handleSignIn = useCallback((data: object) => {
+    console.log(data)
+  }, [])
+
   return (
-    <>
-      <KeyboardAvoidingView style={{ flex: 1 }}behavior={Platform.OS === 'ios' ? 'padding' : undefined} enabled>
-        <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{ flex: 1 }}>
-          <Container>
-            <Image source={logoImg} />
+    <KeyboardAvoidingView style={{ flex: 1 }}behavior={Platform.OS === 'ios' ? 'padding' : undefined} enabled>
+      <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{ flex: 1 }}>
+        <Container>
+          <Image source={logoImg} />
 
-            <View>
-              <Title>Faça seu login</Title>
-            </View>
+          <View>
+            <Title>Faça seu login</Title>
+          </View>
 
+          <Form ref={formRef} onSubmit={handleSignIn}>
             <Input name="email" icon="mail" placeholder="E-mail"/>
             <Input name="password" icon="lock" placeholder="Senha"/>
 
-            <Button onPress={() => { console.log('Deu') }}>Entrar</Button>
-            <ForgotPassword onPress={() => {}}>
-              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
-            </ForgotPassword>
-          </Container>
-        </ScrollView>
-        <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
-          <Icon name="log-in" size={20} color="#ff9000"/>
-          <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
-        </CreateAccountButton>
-      </KeyboardAvoidingView>
-    </>
+            <Button
+              onPress={() => {
+                // eslint-disable-next-line no-unused-expressions
+                formRef.current?.submitForm()
+              }}
+            >
+              Entrar
+            </Button>
+          </Form>
+
+          <ForgotPassword onPress={() => {}}>
+            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+          </ForgotPassword>
+        </Container>
+      </ScrollView>
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
+        <Icon name="log-in" size={20} color="#ff9000"/>
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </KeyboardAvoidingView>
   )
 }
 
