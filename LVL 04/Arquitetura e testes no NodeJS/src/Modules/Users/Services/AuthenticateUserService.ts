5e70e71804d2f479d3385073bf91396835c1f31a
João Lenon <lenonsec7@gmail.com>
2020-06-12 16:05:38 -0300

    feat: NodeJS

--- LVL 04/Arquitetura e testes no NodeJS/src/Modules/Users/Services/AuthenticateUserService.ts
+++ LVL 04/Arquitetura e testes no NodeJS/src/Modules/Users/Services/AuthenticateUserService.ts
@@ -1,9 +1,10 @@
-import { getRepository } from 'typeorm'
 import { compare } from 'bcryptjs'
 import User from '@Modules/Users/Infra/Typeorm/Entities/User'
 import { sign } from 'jsonwebtoken'
 import authConfig from '@Config/auth'
 import AppError from '@Shared/Errors/AppError'
+import IUsersRepository from '@Modules/Users/Repositories/IUsersRepository'
+import { injectable, inject } from 'tsyringe'
 
 interface IRequest {
   email: string
@@ -15,11 +16,15 @@ interface IResponse {
   token: string
 }
 
+@injectable()
 class AuthenticateUserService {
-  public async execute({ email, password }: IRequest): Promise<IResponse> {
-    const usersRepository = getRepository(User)
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) {}
 
-    const user = await usersRepository.findOne({ where: { email } })
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email)
 
     if (!user) {
       throw new AppError('Incorrect email/password combination', 401)
