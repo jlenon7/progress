d199bacbae0d913cc2c525eebf0140f130d86c38
João Lenon <lenonsec7@gmail.com>
2020-06-12 16:05:21 -0300

    feat: NodeJS

---
+++ LVL 04/Arquitetura e testes no NodeJS/src/Modules/Users/Infra/Http/Controllers/UsersController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+
+import UsersService from '@Modules/Users/Services/UsersService'
+
+export default class UsersController {
+  public async store(request: Request, response: Response): Promise<Response> {
+
+  }
+}
