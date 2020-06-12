172750b9232ccc65c74f30eed47816e1c2ba5720
João Lenon <lenonsec7@gmail.com>
2020-06-12 16:05:30 -0300

    feat: NodeJS

--- LVL 04/Arquitetura e testes no NodeJS/src/Modules/Users/Infra/Http/Routes/sessions.routes.ts
+++ LVL 04/Arquitetura e testes no NodeJS/src/Modules/Users/Infra/Http/Routes/sessions.routes.ts
@@ -1,22 +1,9 @@
 import { Router } from 'express'
-
-import AuthenticateUserService from '@Modules/Users/Services/AuthenticateUserService'
+import SessionsController from '@Modules/Users/Infra/Http/Controllers/SessionsController'
 
 const sessionsRouter = Router()
+const sessionsController = new SessionsController()
 
-sessionsRouter.post('/', async (request, response) => {
-  const { email, password } = request.body
-
-  const authenticateUser = new AuthenticateUserService()
-
-  const { user, token } = await authenticateUser.execute({
-    email,
-    password,
-  })
-
-  delete user.password
-
-  return response.json({ user, token })
-})
+sessionsRouter.post('/', sessionsController.store)
 
 export default sessionsRouter
