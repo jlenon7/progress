8ce4842770b65f25cd9cbb9e6eb56c2888772a01
João Lenon <lenonsec7@gmail.com>
2020-06-12 16:05:45 -0300

    feat: NodeJS

---
+++ LVL 04/Arquitetura e testes no NodeJS/src/Shared/Container/index.ts
@@ -0,0 +1,17 @@
+import { container } from 'tsyringe'
+
+import IAppointmentsRepository from '@Modules/Appointments/Repositories/IAppointmentsRepository'
+import AppointmentsRepository from '@Modules/Appointments/Infra/Typeorm/Repositories/AppointmentsRepository'
+
+import IUsersRepository from '@Modules/Users/Repositories/IUsersRepository'
+import UsersRepository from '@Modules/Users/Infra/Typeorm/Repositories/UsersRepository'
+
+container.registerSingleton<IAppointmentsRepository>(
+  'AppointmentsRepository', 
+  AppointmentsRepository
+)
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository', 
+  UsersRepository
+)
