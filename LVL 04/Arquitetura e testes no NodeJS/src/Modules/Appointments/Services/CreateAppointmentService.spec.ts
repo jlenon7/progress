15b24ab8e133e58f7e7ea9f88d26e024a16a070a
João Lenon <lenonsec7@gmail.com>
2020-06-15 11:44:50 -0300

    feat: NodeJS

--- LVL 04/Arquitetura e testes no NodeJS/src/Modules/Appointments/Services/CreateAppointmentService.spec.ts
+++ LVL 04/Arquitetura e testes no NodeJS/src/Modules/Appointments/Services/CreateAppointmentService.spec.ts
@@ -1,5 +1,6 @@
 import FakeAppointmentRepository from '../Repositories/Fakes/FakeAppointmentsRepository'
 import CreateAppointmentService from './CreateAppointmentService'
+import AppError from '@Shared/Errors/AppError'
 
 describe('> Appointments [CREATE]', async () => {
   it('should be able to create a new appointment', async () => {
@@ -15,7 +16,22 @@ describe('> Appointments [CREATE]', async () => {
     expect(appointment.provider_id).toBe('123123')
   })
 
-  // it('should not be able to create two appointments on the same time', () => {
-  //   expect(1 + 2).toBe(3)
-  // })
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository()
+    const createAppointment = new CreateAppointmentService(fakeAppointmentRepository)
+
+    const appointmentDate = new Date(2020, 4, 10, 11)
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123'
+    })
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123123'
+      })
+    ).rejects.toBeInstanceOf(AppError)
+  })
 })
