9787b1d4e4291b377e4f0ab6d80bc7258e548ff3
João Lenon <lenonsec7@gmail.com>
2020-06-15 00:03:05 -0300

    feat: NodeJS

--- LVL 04/Arquitetura e testes no NodeJS/src/Modules/Appointments/Services/CreateAppointmentService.spec.ts
+++ LVL 04/Arquitetura e testes no NodeJS/src/Modules/Appointments/Services/CreateAppointmentService.spec.ts
@@ -1,3 +1,21 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3)
+import FakeAppointmentRepository from '../Repositories/Fakes/FakeAppointmentsRepository'
+import CreateAppointmentService from './CreateAppointmentService'
+
+describe('> Appointments [CREATE]', async () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository()
+    const createAppointment = new CreateAppointmentService(fakeAppointmentRepository)
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123'
+    })
+
+    expect(appointment).toHaveProperty('id')
+    expect(appointment.provider_id).toBe('123123')
+  })
+
+  // it('should not be able to create two appointments on the same time', () => {
+  //   expect(1 + 2).toBe(3)
+  // })
 })
