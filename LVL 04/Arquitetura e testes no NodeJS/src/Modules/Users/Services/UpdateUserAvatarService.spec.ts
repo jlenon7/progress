7317f36bd34e46a479e04d2bc92e20ad0cb67f8c
João Lenon <lenonsec7@gmail.com>
2020-06-16 09:22:23 -0300

    feat: NodeJS

---
+++ LVL 04/Arquitetura e testes no NodeJS/src/Modules/Users/Services/UpdateUserAvatarService.spec.ts
@@ -0,0 +1,75 @@
+import FakeUsersRepository from '../Repositories/Fakes/FakeUsersRepository'
+import FakeStorageProvider from '@Shared/Container/Providers/StorageProvider/Fakes/FakeStorageProvider'
+import UpdateUserAvatarService from './UpdateUserAvatarService'
+import AppError from '@Shared/Errors/AppError'
+
+describe('> Avatar [UPDATE]', () => {
+  it('should be able to update an user avatar', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeStorageProvider = new FakeStorageProvider()
+
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository, 
+      fakeStorageProvider
+    )
+
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456'
+    })
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar.jpg'
+    })
+
+    expect(user.avatar).toBe('avatar.jpg')
+  })
+
+  it('should not be able to update from non existing error', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeStorageProvider = new FakeStorageProvider()
+
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository, 
+      fakeStorageProvider
+    )
+
+    expect(updateUserAvatar.execute({
+      user_id: 'non-existing-user',
+      avatarFilename: 'avatar.jpg'
+    })).rejects.toBeInstanceOf(AppError)
+  })
+
+  it('should delete old avatar when updating new one', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeStorageProvider = new FakeStorageProvider()
+
+    const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile')
+
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository, 
+      fakeStorageProvider
+    )
+
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456'
+    })
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar.jpg'
+    })
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar2.jpg'
+    })
+
+    expect(deleteFile).toHaveBeenCalledWith('avatar.jpg')
+    expect(user.avatar).toBe('avatar2.jpg')
+  })
+})
