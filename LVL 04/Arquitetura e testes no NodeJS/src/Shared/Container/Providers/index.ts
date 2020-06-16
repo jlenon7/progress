26e3635f01852dacab646f728caf97c7a8a89324
João Lenon <lenonsec7@gmail.com>
2020-06-16 09:22:32 -0300

    feat: NodeJS

---
+++ LVL 04/Arquitetura e testes no NodeJS/src/Shared/Container/Providers/index.ts
@@ -0,0 +1,6 @@
+import { container } from 'tsyringe'
+
+import IStorageProvider from './StorageProvider/Models/IStorageProvider'
+import DiskStorageProvider from './StorageProvider/Implementations/DiskStorageProvider'
+
+container.registerSingleton<IStorageProvider>('StorageProvider', DiskStorageProvider)
