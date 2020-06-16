06bb39359a5c5b49c176358caaa816d08b97a076
João Lenon <lenonsec7@gmail.com>
2020-06-16 09:22:21 -0300

    feat: NodeJS

--- LVL 04/Arquitetura e testes no NodeJS/src/Config/upload.ts
+++ LVL 04/Arquitetura e testes no NodeJS/src/Config/upload.ts
@@ -5,12 +5,13 @@ import multer from 'multer'
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp')
 
 export default {
-  directory: tmpFolder,
+  tmpFolder,
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
   storage: multer.diskStorage({
     destination: tmpFolder,
     filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('HEX')
+      const fileHash = crypto.randomBytes(10).toString('hex')
       const fileName = `${fileHash}-${file.originalname}`
 
       return callback(null, fileName)
