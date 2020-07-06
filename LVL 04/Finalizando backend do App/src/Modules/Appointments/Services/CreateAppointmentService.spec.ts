7692efd24def686841de7e225484dcc9cc73c014
João Lenon <lenonsec7@gmail.com>
2020-07-05 23:11:57 -0300

    feat: NodeJS

--- LVL 04/Finalizando backend do App/src/Modules/Appointments/Services/CreateAppointmentService.spec.ts
+++ LVL 04/Finalizando backend do App/src/Modules/Appointments/Services/CreateAppointmentService.spec.ts
@@ -5,11 +5,13 @@ import AppError from '@Shared/Errors/AppError'
 describe('> Appointments [CREATE]', () => {
   it('should be able to create a new appointment', async () => {
     const fakeAppointmentRepository = new FakeAppointmentRepository()
-    const createAppointment = new CreateAppointmentService(fakeAppointmentRepository)
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    )
 
     const appointment = await createAppointment.execute({
       date: new Date(),
-      provider_id: '123123'
+      provider_id: '123123',
     })
 
     expect(appointment).toHaveProperty('id')
@@ -18,20 +20,22 @@ describe('> Appointments [CREATE]', () => {
 
   it('should not be able to create two appointments on the same time', async () => {
     const fakeAppointmentRepository = new FakeAppointmentRepository()
-    const createAppointment = new CreateAppointmentService(fakeAppointmentRepository)
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    )
 
     const appointmentDate = new Date(2020, 4, 10, 11)
 
     await createAppointment.execute({
       date: appointmentDate,
-      provider_id: '123123'
+      provider_id: '123123',
     })
 
-    expect(
+    await expect(
       createAppointment.execute({
         date: appointmentDate,
-        provider_id: '123123'
-      })
+        provider_id: '123123',
+      }),
     ).rejects.toBeInstanceOf(AppError)
   })
 })
