5f3dc3e600d0f52479632f28bff800025af7796a
João Lenon <lenonsec7@gmail.com>
2020-07-05 23:00:36 -0300

    feat: NodeJS

--- LVL 04/Finalizando backend do App/src/Shared/Container/Providers/index.ts
+++ LVL 04/Finalizando backend do App/src/Shared/Container/Providers/index.ts
@@ -3,4 +3,7 @@ import { container } from 'tsyringe'
 import IStorageProvider from './StorageProvider/Models/IStorageProvider'
 import DiskStorageProvider from './StorageProvider/Implementations/DiskStorageProvider'
 
-container.registerSingleton<IStorageProvider>('StorageProvider', DiskStorageProvider)
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  DiskStorageProvider,
+)
