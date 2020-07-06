db5c6fc10caf52fe0daf77c2d8b3a64ddd4e7b29
João Lenon <lenonsec7@gmail.com>
2020-07-05 23:00:28 -0300

    feat: NodeJS

---
+++ LVL 04/Finalizando backend do App/src/Modules/Users/Services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,30 @@
+import FakeUsersRepository from '../Repositories/Fakes/FakeUsersRepository'
+import SendForgotPasswordEmailService from './SendForgotPasswordEmailService'
+import FakeMailProvider from '@Shared/Container/Providers/MailProvider/Fakes/FakeMailProvider'
+// import AppError from '@Shared/Errors/AppError'
+
+describe('> Forgot Password Email [SEND]', () => {
+  it('should be able to recover the password using the email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeMailProvider = new FakeMailProvider()
+
+    const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider,
+    )
+
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail')
+
+    await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    await sendForgotPasswordEmail.execute({
+      email: 'johndoe@example.com',
+    })
+
+    expect(sendMail).toHaveBeenCalled()
+  })
+})
