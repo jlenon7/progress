74c546e5560e16b368afbe4cd47c016c3f01dead
João Lenon <lenonsec7@gmail.com>
2020-07-05 23:11:55 -0300

    feat: NodeJS

--- LVL 04/Finalizando backend do App/src/Modules/Users/Services/UpdateUserAvatarService.spec.ts
+++ LVL 04/Finalizando backend do App/src/Modules/Users/Services/UpdateUserAvatarService.spec.ts
@@ -9,19 +9,19 @@ describe('> Avatar [UPDATE]', () => {
     const fakeStorageProvider = new FakeStorageProvider()
 
     const updateUserAvatar = new UpdateUserAvatarService(
-      fakeUsersRepository, 
-      fakeStorageProvider
+      fakeUsersRepository,
+      fakeStorageProvider,
     )
 
     const user = await fakeUsersRepository.create({
       name: 'John Doe',
       email: 'johndoe@example.com',
-      password: '123456'
+      password: '123456',
     })
 
     await updateUserAvatar.execute({
       user_id: user.id,
-      avatarFilename: 'avatar.jpg'
+      avatarFilename: 'avatar.jpg',
     })
 
     expect(user.avatar).toBe('avatar.jpg')
@@ -32,14 +32,16 @@ describe('> Avatar [UPDATE]', () => {
     const fakeStorageProvider = new FakeStorageProvider()
 
     const updateUserAvatar = new UpdateUserAvatarService(
-      fakeUsersRepository, 
-      fakeStorageProvider
+      fakeUsersRepository,
+      fakeStorageProvider,
     )
 
-    expect(updateUserAvatar.execute({
-      user_id: 'non-existing-user',
-      avatarFilename: 'avatar.jpg'
-    })).rejects.toBeInstanceOf(AppError)
+    await expect(
+      updateUserAvatar.execute({
+        user_id: 'non-existing-user',
+        avatarFilename: 'avatar.jpg',
+      }),
+    ).rejects.toBeInstanceOf(AppError)
   })
 
   it('should delete old avatar when updating new one', async () => {
@@ -49,24 +51,24 @@ describe('> Avatar [UPDATE]', () => {
     const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile')
 
     const updateUserAvatar = new UpdateUserAvatarService(
-      fakeUsersRepository, 
-      fakeStorageProvider
+      fakeUsersRepository,
+      fakeStorageProvider,
     )
 
     const user = await fakeUsersRepository.create({
       name: 'John Doe',
       email: 'johndoe@example.com',
-      password: '123456'
+      password: '123456',
     })
 
     await updateUserAvatar.execute({
       user_id: user.id,
-      avatarFilename: 'avatar.jpg'
+      avatarFilename: 'avatar.jpg',
     })
 
     await updateUserAvatar.execute({
       user_id: user.id,
-      avatarFilename: 'avatar2.jpg'
+      avatarFilename: 'avatar2.jpg',
     })
 
     expect(deleteFile).toHaveBeenCalledWith('avatar.jpg')
