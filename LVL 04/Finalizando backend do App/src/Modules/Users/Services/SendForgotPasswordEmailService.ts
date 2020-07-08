75544779cc59064ba7146bed599c373d59731d42
João Lenon <lenonsec7@gmail.com>
2020-07-08 14:36:29 -0300

    feat: NodeJS

--- LVL 04/Finalizando backend do App/src/Modules/Users/Services/SendForgotPasswordEmailService.ts
+++ LVL 04/Finalizando backend do App/src/Modules/Users/Services/SendForgotPasswordEmailService.ts
@@ -4,6 +4,7 @@ import { injectable, inject } from 'tsyringe'
 import AppError from '@Shared/Errors/AppError'
 import IUsersRepository from '@Modules/Users/Repositories/IUsersRepository'
 import IMailProvider from '@Shared/Container/Providers/MailProvider/Models/IMailProvider'
+import IUserTokensRepository from '@Modules/Users/Repositories/IUserTokensRepository'
 
 interface IRequest {
   email: string
@@ -17,14 +18,19 @@ class SendForgotPasswordEmailService {
 
     @inject('MailProvider')
     private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
-    const checkUserExists = await this.usersRepository.findByEmail(email)
+    const user = await this.usersRepository.findByEmail(email)
 
-    if (!checkUserExists) {
+    if (!user) {
       throw new AppError('User does not exists.')
     }
+
+    await this.userTokensRepository.generate(user.id)
 
     this.mailProvider.sendMail(email, 'Pedido de recuperação de senha recebido')
   }
