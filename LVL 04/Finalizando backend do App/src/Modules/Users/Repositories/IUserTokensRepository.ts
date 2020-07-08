13483a3c8e5b9b0846284ddbd463dcb785844485
João Lenon <lenonsec7@gmail.com>
2020-07-08 15:07:22 -0300

    feat: NodeJS

--- LVL 04/Finalizando backend do App/src/Modules/Users/Repositories/IUserTokensRepository.ts
+++ LVL 04/Finalizando backend do App/src/Modules/Users/Repositories/IUserTokensRepository.ts
@@ -2,4 +2,5 @@ import UserToken from '../Infra/Typeorm/Entities/UserToken'
 
 export default interface IUserTokensRepository {
   generate(user_id: string): Promise<UserToken>
+  findByToken(token: string): Promise<UserToken | undefined>
 }
