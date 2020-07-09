219e95bab0ce5ab0b816be9aa846597648c8ee82
João Lenon <lenonsec7@gmail.com>
2020-07-09 02:04:28 -0300

    feat: NodeJS

--- LVL 04/Continuando backend do App/src/Modules/Users/Services/SendForgotPasswordEmailService.ts
+++ LVL 04/Continuando backend do App/src/Modules/Users/Services/SendForgotPasswordEmailService.ts
@@ -1,5 +1,6 @@
 // import User from '@Modules/Users/Infra/Typeorm/Entities/User'
 import { injectable, inject } from 'tsyringe'
+import path from 'path'
 
 import AppError from '@Shared/Errors/AppError'
 import IUsersRepository from '@Modules/Users/Repositories/IUsersRepository'
@@ -32,6 +33,13 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id)
 
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    )
+
     await this.mailProvider.sendMail({
       to: {
         name: user.name,
@@ -39,10 +47,10 @@ class SendForgotPasswordEmailService {
       },
       subject: '[GoBarber] Recuperação de senha',
       template: {
-        template: 'Olá, {{name}}: {{token}}',
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token,
+          link: `http://localhost:3000/reset_password?token=${token}`,
         },
       },
     })
