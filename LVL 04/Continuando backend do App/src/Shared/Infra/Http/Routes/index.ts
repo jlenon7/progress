cd9486ada331ccbed4222decb481f90823d2063f
João Lenon <lenonsec7@gmail.com>
2020-07-08 23:05:59 -0300

    feat: NodeJS

--- LVL 04/Continuando backend do App/src/Shared/Infra/Http/Routes/index.ts
+++ LVL 04/Continuando backend do App/src/Shared/Infra/Http/Routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express'
 import appointmentsRouter from '@Modules/Appointments/Infra/Http/Routes/appointments.routes'
 import sessionsRouter from '@Modules/Users/Infra/Http/Routes/sessions.routes'
 import usersRouter from '@Modules/Users/Infra/Http/Routes/users.routes'
+import passwordRouter from '@Modules/Users/Infra/Http/Routes/password.routes'
 
 const routes = Router()
 
 routes.use('/appointments', appointmentsRouter)
 routes.use('/sessions', sessionsRouter)
 routes.use('/users', usersRouter)
+routes.use('/password', passwordRouter)
 
 export default routes
