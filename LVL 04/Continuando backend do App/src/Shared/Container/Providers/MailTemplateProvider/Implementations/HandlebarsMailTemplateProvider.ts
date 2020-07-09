c268d8879a871454fd6c51ce9936d15e5b6ae09c
João Lenon <lenonsec7@gmail.com>
2020-07-09 02:04:33 -0300

    feat: NodeJS

--- LVL 04/Continuando backend do App/src/Shared/Container/Providers/MailTemplateProvider/Implementations/HandlebarsMailTemplateProvider.ts
+++ LVL 04/Continuando backend do App/src/Shared/Container/Providers/MailTemplateProvider/Implementations/HandlebarsMailTemplateProvider.ts
@@ -1,14 +1,19 @@
 import handlebars from 'handlebars'
 import IParseMailTemplateDTO from '../Dtos/IParseMailTemplateDTO'
 import IMailTemplateProvider from '../Models/IMailTemplateProvider'
+import fs from 'fs'
 
 export default class HandlebarsMailTemplateProvider
   implements IMailTemplateProvider {
   public async parse({
-    template,
+    file,
     variables,
   }: IParseMailTemplateDTO): Promise<string> {
-    const parseTemplate = handlebars.compile(template)
+    const templateFileContent = await fs.promises.readFile(file, {
+      encoding: 'utf-8',
+    })
+
+    const parseTemplate = handlebars.compile(templateFileContent)
 
     return parseTemplate(variables)
   }
