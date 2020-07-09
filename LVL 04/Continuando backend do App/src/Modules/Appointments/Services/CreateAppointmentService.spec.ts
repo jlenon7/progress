4bfbed7ac55c342cf0e2a05bb1b02389e90fb49f
João Lenon <lenonsec7@gmail.com>
2020-07-09 14:44:15 -0300

    feat: NodeJS

--- LVL 04/Continuando backend do App/src/Modules/Appointments/Services/CreateAppointmentService.spec.ts
+++ LVL 04/Continuando backend do App/src/Modules/Appointments/Services/CreateAppointmentService.spec.ts
@@ -2,13 +2,16 @@ import FakeAppointmentRepository from '../Repositories/Fakes/FakeAppointmentsRep
 import CreateAppointmentService from './CreateAppointmentService'
 import AppError from '@Shared/Errors/AppError'
 
-describe('> Appointments [CREATE]', () => {
-  it('should be able to create a new appointment', async () => {
-    const fakeAppointmentRepository = new FakeAppointmentRepository()
-    const createAppointment = new CreateAppointmentService(
-      fakeAppointmentRepository,
-    )
+let fakeAppointmentRepository: FakeAppointmentRepository
+let createAppointment: CreateAppointmentService
 
+describe('> Appointments [CREATE]', () => {
+  beforeEach(() => {
+    fakeAppointmentRepository = new FakeAppointmentRepository()
+    createAppointment = new CreateAppointmentService(fakeAppointmentRepository)
+  })
+
+  it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
       provider_id: '123123',
@@ -19,11 +22,6 @@ describe('> Appointments [CREATE]', () => {
   })
 
   it('should not be able to create two appointments on the same time', async () => {
-    const fakeAppointmentRepository = new FakeAppointmentRepository()
-    const createAppointment = new CreateAppointmentService(
-      fakeAppointmentRepository,
-    )
-
     const appointmentDate = new Date(2020, 4, 10, 11)
 
     await createAppointment.execute({
