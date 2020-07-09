822b960bbc5f6ac586542fb5fcdb1ab7991d8dbe
João Lenon <lenonsec7@gmail.com>
2020-07-09 02:04:35 -0300

    feat: NodeJS

--- LVL 04/Continuando backend do App/src/Shared/Container/Providers/MailTemplateProvider/Fakes/FakeMailTemplateProvider.ts
+++ LVL 04/Continuando backend do App/src/Shared/Container/Providers/MailTemplateProvider/Fakes/FakeMailTemplateProvider.ts
@@ -1,8 +1,7 @@
-import IParseMailTemplateDTO from '../Dtos/IParseMailTemplateDTO'
 import IMailTemplateProvider from '../Models/IMailTemplateProvider'
 
 export default class FakeMailTemplateProvider implements IMailTemplateProvider {
-  public async parse({ template }: IParseMailTemplateDTO): Promise<string> {
-    return template
+  public async parse(): Promise<string> {
+    return 'Mail content'
   }
 }
