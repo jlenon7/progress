ebd4a9a165620fd83488122890638f68307cc253
João Lenon <lenonsec7@gmail.com>
2020-07-09 01:52:26 -0300

    feat: NodeJS

--- LVL 04/Continuando backend do App/src/Shared/Container/Providers/MailProvider/Fakes/FakeMailProvider.ts
+++ LVL 04/Continuando backend do App/src/Shared/Container/Providers/MailProvider/Fakes/FakeMailProvider.ts
@@ -1,17 +1,10 @@
 import IMailProvider from '../Models/IMailProvider'
-
-interface IMessage {
-  to: string
-  body: string
-}
+import ISendMailDTO from '../Dtos/ISendMailDTO'
 
 export default class FakeMailProvider implements IMailProvider {
-  private messages: IMessage[] = []
+  private messages: ISendMailDTO[] = []
 
-  public async sendMail(to: string, body: string): Promise<void> {
-    this.messages.push({
-      to,
-      body,
-    })
+  public async sendMail(message: ISendMailDTO): Promise<void> {
+    this.messages.push(message)
   }
 }
