85eb906e8397b600291d5c18e7ea5f49d3a09b74
João Lenon <lenonsec7@gmail.com>
2020-07-19 20:50:29 -0300

    feat: NodeJS

--- LVL 04/Continuando backend do App/src/Modules/Appointments/Repositories/IAppointmentsRepository.ts
+++ LVL 04/Continuando backend do App/src/Modules/Appointments/Repositories/IAppointmentsRepository.ts
@@ -1,7 +1,11 @@
 import Appointment from '@Modules/Appointments/Infra/Typeorm/Entities/Appointment'
 import ICreateAppointmentDTO from '@Modules/Appointments/Dtos/ICreateAppointmentDTO'
+import IFindAllInMonthFromProviderDTO from '../Dtos/IFindAllInMonthFromProviderDTO'
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>
   findByDate(date: Date): Promise<Appointment | undefined>
+  findAllInMonthFromProvider(
+    data: IFindAllInMonthFromProviderDTO,
+  ): Promise<Appointment[]>
 }
