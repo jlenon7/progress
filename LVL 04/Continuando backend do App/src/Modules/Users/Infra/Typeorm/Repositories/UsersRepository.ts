502edddfe3284cf1b5e06d2d6736ea35acd605a9
João Lenon <lenonsec7@gmail.com>
2020-07-19 15:33:25 -0300

    feat: NodeJS

--- LVL 04/Continuando backend do App/src/Modules/Users/Infra/Typeorm/Repositories/UsersRepository.ts
+++ LVL 04/Continuando backend do App/src/Modules/Users/Infra/Typeorm/Repositories/UsersRepository.ts
@@ -1,14 +1,31 @@
 import User from '@Modules/Users/Infra/Typeorm/Entities/User'
 import IUsersRepository from '@Modules/Users/Repositories/IUsersRepository'
 import ICreateUserDTO from '@Modules/Users/Dtos/ICreateUserDTO'
+import IFindAllProvidersDTO from '@Modules/Users/Dtos/IFindAllProvidersDTO'
 
-import { getRepository, Repository } from 'typeorm'
+import { getRepository, Repository, Not } from 'typeorm'
 
 class UsersRepository implements IUsersRepository {
   private ormRepository: Repository<User>
 
   constructor() {
     this.ormRepository = getRepository(User)
+  }
+
+  public async findAllProviders({
+    except_user_id,
+  }: IFindAllProvidersDTO): Promise<User[]> {
+    let users: User[]
+
+    if (except_user_id) {
+      users = await this.ormRepository.find({
+        where: { id: Not(except_user_id) },
+      })
+    } else {
+      users = await this.ormRepository.find()
+    }
+
+    return users
   }
 
   public async findById(id: string): Promise<User | undefined> {
