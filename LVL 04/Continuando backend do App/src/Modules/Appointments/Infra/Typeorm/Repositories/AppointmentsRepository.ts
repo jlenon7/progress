1c1bceaad1e93ad663606be215ce0b5e75472ec3
João Lenon <lenonsec7@gmail.com>
2020-07-20 13:59:14 -0300

    feat: NodeJS

--- LVL 04/Continuando backend do App/src/Modules/Appointments/Infra/Typeorm/Repositories/AppointmentsRepository.ts
+++ LVL 04/Continuando backend do App/src/Modules/Appointments/Infra/Typeorm/Repositories/AppointmentsRepository.ts
@@ -2,6 +2,7 @@ import Appointment from '@Modules/Appointments/Infra/Typeorm/Entities/Appointmen
 import IAppointmentsRepository from '@Modules/Appointments/Repositories/IAppointmentsRepository'
 import ICreateAppointmentDTO from '@Modules/Appointments/Dtos/ICreateAppointmentDTO'
 import IFindAllInMonthFromProviderDTO from '@Modules/Appointments/Dtos/IFindAllInMonthFromProviderDTO'
+import IFindAllInDayFromProviderDTO from '@Modules/Appointments/Dtos/IFindAllInDayFromProviderDTO'
 
 import { getRepository, Repository, Raw } from 'typeorm'
 
@@ -33,6 +34,28 @@ class AppointmentsRepository implements IAppointmentsRepository {
         date: Raw(
           dateFieldName =>
             `to_char(${dateFieldName}, 'MM-YYYY') = '${parsedMonth}-${year}'`,
+        ),
+      },
+    })
+
+    return appointments
+  }
+
+  public async findAllInDayFromProvider({
+    provider_id,
+    day,
+    month,
+    year,
+  }: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
+    const parsedDay = String(day).padStart(2, '0')
+    const parsedMonth = String(month).padStart(2, '0')
+
+    const appointments = this.ormRepository.find({
+      where: {
+        provider_id,
+        date: Raw(
+          dateFieldName =>
+            `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
     })
