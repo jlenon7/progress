33fe6f3eba85a90f8373d24b68a670c5e21929a6
João Lenon <lenonsec7@gmail.com>
2020-07-20 15:53:15 -0300

    feat: NodeJS

--- LVL 04/Continuando backend do App/src/Modules/Appointments/Services/ListProviderDayAvailabillityService.spec.ts
+++ LVL 04/Continuando backend do App/src/Modules/Appointments/Services/ListProviderDayAvailabillityService.spec.ts
@@ -17,10 +17,12 @@ describe('> ListProviderDayAvailabillityService', () => {
   it('should be able to list the day availabillity from provider', async () => {
     await fakeAppointmentRepository.create({
       provider_id: 'user',
+      user_id: 'user',
       date: new Date(2020, 4, 20, 14, 0, 0),
     })
     await fakeAppointmentRepository.create({
       provider_id: 'user',
+      user_id: 'user',
       date: new Date(2020, 4, 20, 15, 0, 0),
     })
 
