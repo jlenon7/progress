8c735012bb3f892788232bbdf0c00be8fd627504
João Lenon <lenonsec7@gmail.com>
2020-07-25 16:14:27 -0300

    feat: NodeJS

--- LVL 04/Finalizando back-end do app/src/Modules/Appointments/Infra/Http/Routes/appointments.routes.ts
+++ LVL 04/Finalizando back-end do app/src/Modules/Appointments/Infra/Http/Routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { celebrate, Segments, Joi } from 'celebrate'
 import ensureAuthenticated from '@Modules/Users/Infra/Http/Middlewares/ensureAuthenticated'
 import AppointmentsController from '@Modules/Appointments/Infra/Http/Controllers/AppointmentsController'
 import ProviderAppointmentsController from '../Controllers/ProviderAppointmentsController'
@@ -8,7 +9,16 @@ const appointmentsController = new AppointmentsController()
 const providerAppointmentsController = new ProviderAppointmentsController()
 
 appointmentsRouter.use(ensureAuthenticated)
-appointmentsRouter.post('/', appointmentsController.store)
+appointmentsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      provider_id: Joi.string().uuid().required(),
+      date: Joi.date(),
+    },
+  }),
+  appointmentsController.store,
+)
 appointmentsRouter.get('/me', providerAppointmentsController.index)
 
 export default appointmentsRouter
