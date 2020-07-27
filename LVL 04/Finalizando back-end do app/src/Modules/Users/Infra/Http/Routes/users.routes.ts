0d1a19077eb1a418303ad62ef169b45fed082f95
João Lenon <lenonsec7@gmail.com>
2020-07-27 01:07:28 -0300

    feat: NodeJS

--- LVL 04/Finalizando back-end do app/src/Modules/Users/Infra/Http/Routes/users.routes.ts
+++ LVL 04/Finalizando back-end do app/src/Modules/Users/Infra/Http/Routes/users.routes.ts
@@ -11,7 +11,7 @@ import UserAvatarController from '@Modules/Users/Infra/Http/Controllers/UserAvat
 const usersRouter = Router()
 const usersController = new UsersController()
 const userAvatarController = new UserAvatarController()
-const upload = multer(uploadConfig)
+const upload = multer(uploadConfig.multer)
 
 usersRouter.post(
   '/',
