5e87a08319eb9bb4fd5eb453e01e4e2fabc0c823
João Lenon <lenonsec7@gmail.com>
2020-07-26 23:59:59 -0300

    feat: NodeJS

--- LVL 04/Finalizando back-end do app/src/Shared/Container/Providers/index.ts
+++ LVL 04/Finalizando back-end do app/src/Shared/Container/Providers/index.ts
@@ -1,29 +1,3 @@
-import { container } from 'tsyringe'
-import mailConfig from '@Config/mail'
-
-import IStorageProvider from './StorageProvider/Models/IStorageProvider'
-import DiskStorageProvider from './StorageProvider/Implementations/DiskStorageProvider'
-
-import IMailProvider from './MailProvider/Models/IMailProvider'
-import SESMailProvider from './MailProvider/Implementations/SESMailProvider'
-import EtherealMailProvider from './MailProvider/Implementations/EtherealMailProvider'
-
-import IMailTemplateProvider from './MailTemplateProvider/Models/IMailTemplateProvider'
-import HandlebarsMailTemplateProvider from './MailTemplateProvider/Implementations/HandlebarsMailTemplateProvider'
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-)
-
-container.registerSingleton<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  HandlebarsMailTemplateProvider,
-)
-
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  mailConfig.driver === 'ethereal'
-    ? container.resolve(EtherealMailProvider)
-    : container.resolve(SESMailProvider),
-)
+import './StorageProvider'
+import './MailTemplateProvider'
+import './MailProvider'
