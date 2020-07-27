5fca414c337544f62100dce7fa553c946550e770
João Lenon <lenonsec7@gmail.com>
2020-07-27 01:54:58 -0300

    feat: NodeJS

--- LVL 04/Finalizando back-end do app/src/Modules/Appointments/Services/ListProvidersService.ts
+++ LVL 04/Finalizando back-end do app/src/Modules/Appointments/Services/ListProvidersService.ts
@@ -1,6 +1,8 @@
 import { injectable, inject } from 'tsyringe'
 
 import IUsersRepository from '@Modules/Users/Repositories/IUsersRepository'
+import ICacheProvider from '@Shared/Container/Providers/CacheProvider/Models/ICacheProvider'
+
 import User from '@Modules/Users/Infra/Typeorm/Entities/User'
 
 interface IRequest {
@@ -12,12 +14,23 @@ class ListProvidersService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    const users = await this.usersRepository.findAllProviders({
-      except_user_id: user_id,
-    })
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${user_id}`,
+    )
+
+    if (!users) {
+      users = await this.usersRepository.findAllProviders({
+        except_user_id: user_id,
+      })
+    }
+
+    await this.cacheProvider.save(`providers-list:${user_id}`, users)
 
     return users
   }
