c0940e645729a56ea3c1946b6c9f9f578b147635
João Lenon <lenonsec7@gmail.com>
2020-08-01 15:25:45 -0300

    feat: NodeJS

--- LVL 04/Finalizando back-end do app/src/Modules/Appointments/Repositories/Fakes/FakeAppointmentsRepository.ts
+++ LVL 04/Finalizando back-end do app/src/Modules/Appointments/Repositories/Fakes/FakeAppointmentsRepository.ts
@@ -10,9 +10,14 @@ import IFindAllInDayFromProviderDTO from '@Modules/Appointments/Dtos/IFindAllInD
 class AppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = []
 
-  public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = this.appointments.find(appointment =>
-      isEqual(appointment.date, date),
+  public async findByDate(
+    date: Date,
+    provider_id: string,
+  ): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(
+      appointment =>
+        isEqual(appointment.date, date) &&
+        appointment.provider_id === provider_id,
     )
 
     return findAppointment
