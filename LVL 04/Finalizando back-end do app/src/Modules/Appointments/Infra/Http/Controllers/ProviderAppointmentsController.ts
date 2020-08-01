d3731504ff3d5a95b41f4d331ae86679502a485d
João Lenon <lenonsec7@gmail.com>
2020-08-01 15:25:24 -0300

    feat: NodeJS

--- LVL 04/Finalizando back-end do app/src/Modules/Appointments/Infra/Http/Controllers/ProviderAppointmentsController.ts
+++ LVL 04/Finalizando back-end do app/src/Modules/Appointments/Infra/Http/Controllers/ProviderAppointmentsController.ts
@@ -2,23 +2,24 @@ import { Request, Response } from 'express'
 import { container } from 'tsyringe'
 
 import ListProviderAppointmentsService from '@Modules/Appointments/Services/ListProviderAppointmentsService'
+import { classToClass } from 'class-transformer'
 
 export default class ProviderAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const provider_id = request.user.id
-    const { day, month, year } = request.body
+    const { day, month, year } = request.query
 
     const listProviderAppointments = container.resolve(
       ListProviderAppointmentsService,
     )
 
-    const appointment = await listProviderAppointments.execute({
+    const appointments = await listProviderAppointments.execute({
       provider_id,
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     })
 
-    return response.json(appointment)
+    return response.json(classToClass(appointments))
   }
 }
