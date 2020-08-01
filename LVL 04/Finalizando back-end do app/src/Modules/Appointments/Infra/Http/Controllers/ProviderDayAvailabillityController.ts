72d9a5d384039874d00601394f3193b5237ed46c
João Lenon <lenonsec7@gmail.com>
2020-08-01 15:25:28 -0300

    feat: NodeJS

--- LVL 04/Finalizando back-end do app/src/Modules/Appointments/Infra/Http/Controllers/ProviderDayAvailabillityController.ts
+++ LVL 04/Finalizando back-end do app/src/Modules/Appointments/Infra/Http/Controllers/ProviderDayAvailabillityController.ts
@@ -6,7 +6,7 @@ import ListProviderDayAvailabillityService from '@Modules/Appointments/Services/
 export default class ProviderDayAvailabillityController {
   public async index(request: Request, response: Response): Promise<Response> {
     const { provider_id } = request.params
-    const { day, month, year } = request.body
+    const { day, month, year } = request.query
 
     const listProviderDayAvailabillity = container.resolve(
       ListProviderDayAvailabillityService,
@@ -14,9 +14,9 @@ export default class ProviderDayAvailabillityController {
 
     const availabillity = await listProviderDayAvailabillity.execute({
       provider_id,
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     })
 
     return response.json({ availabillity })
