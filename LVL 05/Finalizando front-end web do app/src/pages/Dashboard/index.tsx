02e4930031937fd2337b81d8416d22f0eae5a52e
João Lenon <lenonsec7@gmail.com>
2020-08-02 15:34:30 -0300

    feat: ReactJS

--- LVL 05/Finalizando front-end web do app/src/pages/Dashboard/index.tsx
+++ LVL 05/Finalizando front-end web do app/src/pages/Dashboard/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState, useCallback, useEffect, useMemo } from 'react'
+import DayPicker, { DayModifiers } from 'react-day-picker'
+import 'react-day-picker/lib/style.css'
 
 import {
   Container,
@@ -8,14 +10,64 @@ import {
   Content,
   Schedule,
   NextAppointment,
+  Section,
+  Appointment,
   Calender,
 } from './styles'
 import logoImg from '../../assets/logo.svg'
 import { FiPower, FiClock } from 'react-icons/fi'
 import { useAuth } from '../../hooks/auth'
+import api from '../../services/apiClient'
+
+interface MonthAvailabillityItem {
+  day: number
+  available: boolean
+}
 
 const Dashboard: React.FC = () => {
   const { signOut, user } = useAuth()
+
+  const [selectedDate, setSelectedDate] = useState(new Date())
+  const [currentMonth, setCurrentMonth] = useState(new Date())
+  const [monthAvailabillity, setMonthAvailabillity] = useState<
+    MonthAvailabillityItem[]
+  >([])
+
+  const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if (modifiers.available) {
+      setSelectedDate(day)
+    }
+  }, [])
+
+  const handleMonthChange = useCallback((month: Date) => {
+    setCurrentMonth(month)
+  }, [])
+
+  useEffect(() => {
+    api
+      .get(`/providers/${user.id}/month-availability`, {
+        params: {
+          year: currentMonth.getFullYear(),
+          month: currentMonth.getMonth() + 1,
+        },
+      })
+      .then(response => {
+        setMonthAvailabillity(response.data)
+      })
+  }, [currentMonth, user.id])
+
+  const disabledDays = useMemo(() => {
+    const dates = monthAvailabillity
+      .filter(monthDay => monthDay.available === false)
+      .map(monthDay => {
+        const year = currentMonth.getFullYear()
+        const month = currentMonth.getMonth()
+
+        return new Date(year, month, monthDay.day)
+      })
+
+    return dates
+  }, [currentMonth, monthAvailabillity])
 
   return (
     <Container>
@@ -61,8 +113,104 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/52840235?s=460&u=f390ab598011bdfe2bc4f4712b7781234c2b1dac&v=4"
+                  alt="João Lenon"
+                />
+
+                <strong>João Lenon</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/52840235?s=460&u=f390ab598011bdfe2bc4f4712b7781234c2b1dac&v=4"
+                  alt="João Lenon"
+                />
+
+                <strong>João Lenon</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/52840235?s=460&u=f390ab598011bdfe2bc4f4712b7781234c2b1dac&v=4"
+                  alt="João Lenon"
+                />
+
+                <strong>João Lenon</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/52840235?s=460&u=f390ab598011bdfe2bc4f4712b7781234c2b1dac&v=4"
+                  alt="João Lenon"
+                />
+
+                <strong>João Lenon</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
-        <Calender />
+        <Calender>
+          <DayPicker
+            weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+            fromMonth={new Date()}
+            disabledDays={[{ daysOfWeek: [0, 6] }, ...disabledDays]}
+            modifiers={{
+              available: { daysOfWeek: [1, 2, 3, 4, 5] },
+            }}
+            onMonthChange={handleMonthChange}
+            selectedDays={selectedDate}
+            onDayClick={handleDateChange}
+            months={[
+              'Janeiro',
+              'Fevereiro',
+              'Março',
+              'Abril',
+              'Maio',
+              'Junho',
+              'Julho',
+              'Agosto',
+              'Setembro',
+              'Outubro',
+              'Novembro',
+              'Dezembro',
+            ]}
+          />
+        </Calender>
       </Content>
     </Container>
   )
