59b429ee24e2881dea9df76c27c0e03f40950a0b
João Lenon <lenonsec7@gmail.com>
2020-08-02 21:35:00 -0300

    feat: ReactJS

--- LVL 05/Finalizando front-end web do app/src/pages/Dashboard/styles.ts
+++ LVL 05/Finalizando front-end web do app/src/pages/Dashboard/styles.ts
@@ -135,6 +135,7 @@ export const NextAppointment = styled.div`
       display: flex;
       align-items: center;
       color: #999591;
+      width: 70px;
 
       svg {
         color: #ff9000;
@@ -155,6 +156,10 @@ export const Section = styled.section`
     display: block;
     padding-bottom: 16px;
     margin-bottom: 16px;
+  }
+
+  > p {
+    color: #999591;
   }
 `
 
