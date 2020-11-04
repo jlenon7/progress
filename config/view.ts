ad3c78aaadb2e7fbc1ed09b890a2ef284a025bfc
João Lenon <lenonsec7@gmail.com>
2020-11-04 14:43:10 -0300

    refactor: Project Structure Refactor

---
+++ config/view.ts
@@ -0,0 +1,16 @@
+export default {
+  /*
+  |--------------------------------------------------------------------------
+  | View Storage Paths
+  |--------------------------------------------------------------------------
+  |
+  | Most templating systems load templates from disk. Here you may specify
+  | an array of paths that should be checked for your views. Of course
+  | the usual Laravel view path has already been registered for you.
+  |
+  */
+
+  paths: {
+    mail: [process.cwd() + '/src/app/views/mail/'],
+  },
+}
