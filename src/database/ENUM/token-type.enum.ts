76f306070cea1a903ca5110f6d23708f63390084
João Lenon <lenonsec7@gmail.com>
2020-11-04 14:45:06 -0300

    refactor: Project Structure Refactor

--- src/database/ENUM/token-type.enum.ts
+++
@@ -1,8 +0,0 @@
-enum TokenTypeEnum {
-  REFRESH_TOKEN = 'refresh_token',
-  ACCESS_TOKEN = 'access_token',
-  EMAIL_CONFIRMATION = 'email_confirmation',
-  FORGOT_PASSWORD = 'forgot_password',
-}
-
-export default TokenTypeEnum
