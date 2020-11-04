9ec862be8bbe70414bfa29a4e1a9f3dd8b965fd9
João Lenon <lenonsec7@gmail.com>
2020-11-04 14:44:58 -0300

    refactor: Project Structure Refactor

--- src/config/orm.ts
+++
@@ -1,4 +0,0 @@
-import UserRepository from '../app/repositories/user.repository'
-import UserTokenRepository from '../app/repositories/user.token.repository'
-
-export default [UserRepository, UserTokenRepository]
