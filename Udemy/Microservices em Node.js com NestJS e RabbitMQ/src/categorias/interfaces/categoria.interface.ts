c0a56443d43b1fd2dfa415fb1d2d944824b2133f
João Lenon <lenonsec7@gmail.com>
2020-11-22 18:42:24 -0300

    feat: Microservices NestJS e RabbitMQ

---
+++ Udemy/Microservices em Node.js com NestJS e RabbitMQ/src/categorias/interfaces/categoria.interface.ts
@@ -0,0 +1,15 @@
+import { Document } from "mongoose";
+import { IJogador } from "src/jogadores/interfaces/jogador.interface";
+
+export interface ICategoria extends Document {
+  readonly categoria: string
+  descricao: string
+  eventos: IEvento[]
+  jogadores: IJogador[]
+}
+
+export interface IEvento {
+  nome: string
+  operacao: string
+  valor: number
+}
