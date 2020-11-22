acb649d90c6e2915ab97e706ec02a789f633604c
João Lenon <lenonsec7@gmail.com>
2020-11-22 14:20:52 -0300

    feat: Microservices NestJS e RabbitMQ

--- Udemy/Microservices em Node.js com NestJS e RabbitMQ/src/app.module.ts
+++ Udemy/Microservices em Node.js com NestJS e RabbitMQ/src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose'
 import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://admin:0yFrMqZbtFJ4fH3S@cluster0.aejvg.mongodb.net/development?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }),
+    JogadoresModule
+  ],
   controllers: [],
   providers: [],
 })
