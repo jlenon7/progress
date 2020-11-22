68c13faec68a9a48960392d18438026b5d95185b
João Lenon <lenonsec7@gmail.com>
2020-11-22 18:42:26 -0300

    feat: Microservices NestJS e RabbitMQ

---
+++ Udemy/Microservices em Node.js com NestJS e RabbitMQ/src/categorias/interfaces/categoria.schema.ts
@@ -0,0 +1,17 @@
+import { Schema } from "mongoose";
+
+export const CategoriaSchema = new Schema({
+  categoria: { type: String, unique: true },
+  descricao: String,
+  eventos: [
+    {
+      nome: String,
+      operacao: String,
+      valor: Number
+    }
+  ],
+  jogadores: [{
+    type: Schema.Types.ObjectId,
+    ref: "Jogador"
+  }]
+}, { timestamps: true, collection: 'categorias' })
