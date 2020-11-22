3b5f160e02ce7e305e9c74fb9854c9320b1fa492
João Lenon <lenonsec7@gmail.com>
2020-11-22 12:50:18 -0300

    feat: Microservices NestJS e RabbitMQ

--- Udemy/Microservices em Node.js com NestJS e RabbitMQ/src/jogadores/jogadores.module.ts
+++ Udemy/Microservices em Node.js com NestJS e RabbitMQ/src/jogadores/jogadores.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { JogadoresController } from './jogadores.controller';
+import { JogadoresService } from './jogadores.service';
 
 @Module({
-  controllers: [JogadoresController]
+  controllers: [JogadoresController],
+  providers: [JogadoresService]
 })
 export class JogadoresModule {}
