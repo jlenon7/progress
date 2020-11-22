4e04faed2e576bed9ea160ec80b90c4635da9d86
João Lenon <lenonsec7@gmail.com>
2020-11-22 13:52:17 -0300

    feat: Microservices NestJS e RabbitMQ

--- Udemy/Microservices em Node.js com NestJS e RabbitMQ/src/jogadores/jogadores.controller.ts
+++ Udemy/Microservices em Node.js com NestJS e RabbitMQ/src/jogadores/jogadores.controller.ts
@@ -1,14 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { IJogador } from './interfaces/jogador.interface';
 import { JogadoresService } from './jogadores.service';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
   constructor(private readonly jogadoresService: JogadoresService) {}
+  
   @Post()
   async criarAtualizarJogador(
     @Body() criarJogadorDto: CriarJogadorDto
   ): Promise<void> {
     return this.jogadoresService.criarAtualizarJogador(criarJogadorDto)
+  }
+
+  @Get()
+  async consultarJogadores(): Promise<IJogador[]> {
+    return this.jogadoresService.consultarJogadores()
   }
 }
