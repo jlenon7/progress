c9015ca19c782bac9a1a257414cba6bbd55d2fe5
João Lenon <lenonsec7@gmail.com>
2020-11-22 16:47:37 -0300

    feat: Microservices NestJS e RabbitMQ

--- Udemy/Microservices em Node.js com NestJS e RabbitMQ/src/jogadores/interfaces/jogador.schema.ts
+++ Udemy/Microservices em Node.js com NestJS e RabbitMQ/src/jogadores/interfaces/jogador.schema.ts
@@ -1,10 +1,10 @@
 import { Schema } from 'mongoose'
 
 export const JogadorSchema = new Schema({
-  telefoneCelular: { type: String, unique: true },
   email: { type: String, unique: true },
   nome: String,
   ranking: String,
   posicaoRanking: Number,
   urlFotoJogador: String,
+  telefoneCelular: String,
 }, { timestamps: true, collection: 'jogadores' })
