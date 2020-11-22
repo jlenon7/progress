b2ecb96c1b1fa3f65b0a8f7fc9244080cbc2be3c
João Lenon <lenonsec7@gmail.com>
2020-11-22 12:39:19 -0300

    feat: Microservices NestJS e RabbitMQ

---
+++ Udemy/Microservices em Node.js com NestJS e RabbitMQ/src/jogadores/interfaces/jogador.interface.ts
@@ -0,0 +1,9 @@
+export interface IJogador {
+  readonly _id: string
+  readonly telefoneCelular: string
+  readonly email: string
+  nome: string
+  ranking: string
+  posicaoRanking: number
+  urlFotoJogador: string
+}
