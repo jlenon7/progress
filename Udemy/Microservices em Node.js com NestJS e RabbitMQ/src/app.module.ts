d132113a289b5e11b48418d63d554403c1ea595a
João Lenon <lenonsec7@gmail.com>
2020-11-24 19:45:28 -0300

    feat: Microservices NestJS e RabbitMQ

--- Udemy/Microservices em Node.js com NestJS e RabbitMQ/src/app.module.ts
+++ Udemy/Microservices em Node.js com NestJS e RabbitMQ/src/app.module.ts
@@ -1,16 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
-import { JogadoresModule } from './jogadores/jogadores.module';
-import { CategoriasModule } from './categorias/categorias.module';
-import { DesafiosModule } from './desafios/desafios.module';
+import { JogadoresModule } from './jogadores/jogadores.module'
+import { CategoriasModule } from './categorias/categorias.module'
+import { DesafiosModule } from './desafios/desafios.module'
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://admin:0yFrMqZbtFJ4fH3S@cluster0.aejvg.mongodb.net/development?retryWrites=true&w=majority',
-    { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }),
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:0yFrMqZbtFJ4fH3S@cluster0.aejvg.mongodb.net/development?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      },
+    ),
     JogadoresModule,
     CategoriasModule,
-    DesafiosModule
+    DesafiosModule,
   ],
   controllers: [],
   providers: [],
