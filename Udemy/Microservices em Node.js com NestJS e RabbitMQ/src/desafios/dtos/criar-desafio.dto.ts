4c2cba9c89d73cad09cfef24392519793dabb63d
João Lenon <lenonsec7@gmail.com>
2020-11-24 09:08:37 -0300

    feat: Microservices NestJS e RabbitMQ

---
+++ Udemy/Microservices em Node.js com NestJS e RabbitMQ/src/desafios/dtos/criar-desafio.dto.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty, IsDate, IsArray, ArrayMinSize, ArrayMaxSize, IsDateString } from 'class-validator';
+import { IJogador } from 'src/jogadores/interfaces/jogador.interface';
+
+export class CriarDesafioDto {
+  @IsNotEmpty()
+  @IsDateString()
+  dataHoraDesafio: Date;
+
+  @IsNotEmpty()
+  solicitante: IJogador;
+
+  @IsArray()
+  @ArrayMinSize(2)
+  @ArrayMaxSize(2)
+  jogadores: IJogador[]
+
+
+}
