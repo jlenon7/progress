0c3aced040499675c75dad3f62d1b0559cc50c61
João Lenon <lenonsec7@gmail.com>
2020-11-24 20:26:46 -0300

    feat: Microservices NestJS e RabbitMQ

--- Udemy/Microservices em Node.js com NestJS e RabbitMQ/src/desafios/desafios.module.ts
+++ Udemy/Microservices em Node.js com NestJS e RabbitMQ/src/desafios/desafios.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common'
 import { DesafiosService } from './desafios.service'
 import { DesafiosController } from './desafios.controller'
+import { MongooseModule } from '@nestjs/mongoose'
+import { DesafioSchema } from './interfaces/desafio.schema'
+import { PartidaSchema } from './interfaces/partida.schema'
+import { JogadoresModule } from 'src/jogadores/jogadores.module'
+import { CategoriasModule } from 'src/categorias/categorias.module'
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'Desafio', schema: DesafioSchema },
+      { name: 'Partida', schema: PartidaSchema },
+    ]),
+    JogadoresModule,
+    CategoriasModule,
+  ],
   controllers: [DesafiosController],
   providers: [DesafiosService],
 })
