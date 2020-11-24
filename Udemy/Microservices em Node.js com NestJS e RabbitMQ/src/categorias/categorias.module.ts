1d179acafdccff45f3fa1c50f60afe2b693d318a
João Lenon <lenonsec7@gmail.com>
2020-11-24 20:26:41 -0300

    feat: Microservices NestJS e RabbitMQ

--- Udemy/Microservices em Node.js com NestJS e RabbitMQ/src/categorias/categorias.module.ts
+++ Udemy/Microservices em Node.js com NestJS e RabbitMQ/src/categorias/categorias.module.ts
@@ -12,5 +12,6 @@ import { CategoriaSchema } from './interfaces/categoria.schema'
   ],
   controllers: [CategoriasController],
   providers: [CategoriasService],
+  exports: [CategoriasService],
 })
 export class CategoriasModule {}
