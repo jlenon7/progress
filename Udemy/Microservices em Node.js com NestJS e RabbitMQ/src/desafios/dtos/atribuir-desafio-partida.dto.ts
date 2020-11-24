2f03323be1fdd2eb3be675a689732c16ad6ed76b
João Lenon <lenonsec7@gmail.com>
2020-11-24 09:08:32 -0300

    feat: Microservices NestJS e RabbitMQ

---
+++ Udemy/Microservices em Node.js com NestJS e RabbitMQ/src/desafios/dtos/atribuir-desafio-partida.dto.ts
@@ -0,0 +1,14 @@
+import { IsNotEmpty } from 'class-validator';
+import { IResultado } from '../interfaces/desafio.interface';
+import { IJogador } from '../../jogadores/interfaces/jogador.interface'
+
+
+export class AtribuirDesafioPartidaDto {
+
+  @IsNotEmpty()
+  def: IJogador
+
+  @IsNotEmpty()
+  resultado: IResultado
+  
+}
