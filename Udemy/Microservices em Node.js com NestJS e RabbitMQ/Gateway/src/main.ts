95333895d595d437de0bc71a49e466eeda283c27
João Lenon <lenonsec7@gmail.com>
2020-11-24 23:43:45 -0300

    feat: Microservices NestJS e RabbitMQ

--- Udemy/Microservices em Node.js com NestJS e RabbitMQ/Gateway/src/main.ts
+++ Udemy/Microservices em Node.js com NestJS e RabbitMQ/Gateway/src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module'
+import { NestFactory } from '@nestjs/core'
+import * as momentTimezone from 'moment-timezone'
+import { AllExceptionFilter } from './filters/http-exception.filter'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  app.useGlobalFilters(new AllExceptionFilter())
+
+  // eslint-disable-next-line no-extend-native
+  Date.prototype.toJSON = () => {
+    return momentTimezone(this)
+      .tz('America/Sao_Paulo')
+      .format('YYYY-MM-DD HH:mm:ss.SSS')
+  }
+
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
