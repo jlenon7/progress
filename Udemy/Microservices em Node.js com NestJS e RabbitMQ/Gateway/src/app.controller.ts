779e7c2a807a682f7fe5f1c080abc196148acaf5
João Lenon <lenonsec7@gmail.com>
2020-11-24 23:43:38 -0300

    feat: Microservices NestJS e RabbitMQ

--- Udemy/Microservices em Node.js com NestJS e RabbitMQ/Gateway/src/app.controller.ts
+++ Udemy/Microservices em Node.js com NestJS e RabbitMQ/Gateway/src/app.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices'
+import {
+  Controller,
+  Post,
+  Logger,
+  ValidationPipe,
+  Body,
+  UsePipes,
+} from '@nestjs/common'
+import { CriarCategoriaDto } from './dtos/criar-categoria.dto'
 
-@Controller()
+@Controller('api/v1')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private logger = new Logger(AppController.name)
+  private clientAdminBackend: ClientProxy
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  constructor() {
+    this.clientAdminBackend = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: [
+          'amqp://rfwsurug:5QzatDz5SCjTSNzP4FnfLTjTzBzVdK8m@jaguar.rmq.cloudamqp.com:5672/rfwsurug',
+        ],
+        queue: 'admin-backend',
+      },
+    })
+  }
+
+  @Post('categorias')
+  @UsePipes(ValidationPipe)
+  async criarCategoria(@Body() categoriaDto: CriarCategoriaDto) {
+    return this.clientAdminBackend.emit('criar-categoria', categoriaDto)
   }
 }
