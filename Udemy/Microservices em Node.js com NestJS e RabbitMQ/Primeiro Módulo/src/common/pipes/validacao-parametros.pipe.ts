e22d5021f30e7e3fe0a597d527c18a4f971996cf
João Lenon <lenonsec7@gmail.com>
2020-11-24 21:23:01 -0300

    feat: Microservices NestJS e RabbitMQ

---
+++ Udemy/Microservices em Node.js com NestJS e RabbitMQ/Primeiro Módulo/src/common/pipes/validacao-parametros.pipe.ts
@@ -0,0 +1,17 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common'
+
+export class ValidacaoParametrosPipe implements PipeTransform {
+  transform(value: string, metadata: ArgumentMetadata): string {
+    if (!value) {
+      throw new BadRequestException(
+        `O valor do parametro ${metadata.data} deve ser informado`,
+      )
+    }
+
+    return value
+  }
+}
