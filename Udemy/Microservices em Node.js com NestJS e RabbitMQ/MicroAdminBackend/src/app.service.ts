b52aa01ddb1b55e1260737bde30e933d1d31d002
João Lenon <lenonsec7@gmail.com>
2020-11-26 21:01:06 -0300

    feat: Microservices NestJS e RabbitMQ

---
+++ Udemy/Microservices em Node.js com NestJS e RabbitMQ/MicroAdminBackend/src/app.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, Logger } from '@nestjs/common'
+import { RpcException } from '@nestjs/microservices'
+import { InjectModel } from '@nestjs/mongoose/dist/common/mongoose.decorators'
+import { Model } from 'mongoose'
+import { ICategoria } from './interfaces/categorias/categoria.interface'
+import { IJogador } from './interfaces/jogadores/jogador.interface'
+
+@Injectable()
+export class AppService {
+  private logger = new Logger('AppService')
+
+  @InjectModel('Categoria') private readonly categoriaModel: Model<ICategoria>
+  @InjectModel('Jogador') private readonly jogadorModel: Model<IJogador>
+
+  public async getCategoria(id: string): Promise<ICategoria> {
+    try {
+      return this.categoriaModel.findOne({ _id: id }).exec()
+    } catch (error) {
+      this.logger.error(`Error: ${JSON.stringify(error.message)}`)
+
+      throw new RpcException(error.message)
+    }
+  }
+
+  public async getCategorias(): Promise<ICategoria[]> {
+    try {
+      return this.categoriaModel.find().exec()
+    } catch (error) {
+      this.logger.error(`Error: ${JSON.stringify(error.message)}`)
+
+      throw new RpcException(error.message)
+    }
+  }
+
+  public async setCategoria(categoria: ICategoria): Promise<ICategoria> {
+    try {
+      return new this.categoriaModel(categoria).save()
+    } catch (error) {
+      this.logger.error(`Error: ${JSON.stringify(error.message)}`)
+
+      throw new RpcException(error.message)
+    }
+  }
+}
