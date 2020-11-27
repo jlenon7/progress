db9ef1919f10e7a475f01a87b664df5f02caa4b2
João Lenon <lenonsec7@gmail.com>
2020-11-26 23:12:09 -0300

    feat: Microservices NestJS e RabbitMQ

--- Udemy/Microservices em Node.js com NestJS e RabbitMQ/MicroAdminBackend/src/app.module.ts
+++ Udemy/Microservices em Node.js com NestJS e RabbitMQ/MicroAdminBackend/src/app.module.ts
@@ -1,24 +1,23 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CategoriaSchema } from './interfaces/categorias/categoria.schema';
-import { JogadorSchema } from './interfaces/jogadores/jogador.schema';
+import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { CategoriasModule } from './categorias/categorias.module'
+import { JogadoresModule } from './jogadores/jogadores.module'
 
 @Module({
-  imports: [MongooseModule.forRoot(
-    'mongodb+srv://admin:0yFrMqZbtFJ4fH3S@cluster0.aejvg.mongodb.net/micro-adm?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    },
-  ),
-  MongooseModule.forFeature([{ name: 'Jogador', schema: JogadorSchema }, { name: 'Categoria', schema: CategoriaSchema }]),
-  
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:0yFrMqZbtFJ4fH3S@cluster0.aejvg.mongodb.net/micro-adm?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      },
+    ),
+    CategoriasModule,
+    JogadoresModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
