e3df3be15dcbf44f3cb722273a537ad04ed1f04b
João Lenon <lenonsec7@gmail.com>
2020-11-26 23:11:45 -0300

    feat: Microservices NestJS e RabbitMQ

---
+++ Udemy/Microservices em Node.js com NestJS e RabbitMQ/Gateway/src/jogadores/dtos/atualizar-jogador.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, IsOptional } from 'class-validator'
+
+export class AtualizarJogadorDto {
+  @IsNotEmpty()
+  readonly telefoneCelular: string
+
+  @IsNotEmpty()
+  readonly nome: string
+
+  @IsOptional()
+  categoria: string
+}
