a9944d3557d845a9c870992bb8a4c54dada48679
João Lenon <lenonsec7@gmail.com>
2020-11-26 22:37:38 -0300

    feat: Microservices NestJS e RabbitMQ

--- Udemy/Microservices em Node.js com NestJS e RabbitMQ/MicroAdminBackend/src/app.controller.ts
+++ Udemy/Microservices em Node.js com NestJS e RabbitMQ/MicroAdminBackend/src/app.controller.ts
@@ -1,23 +1,67 @@
 import { Controller, Get, Logger } from '@nestjs/common'
-import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices'
+import {
+  Ctx,
+  EventPattern,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices'
 import { AppService } from './app.service'
 import { ICategoria } from './interfaces/categorias/categoria.interface'
 
 @Controller()
 export class AppController {
+  private ackErrors = ['E11000']
   constructor(private readonly appService: AppService) {}
 
-  @EventPattern('criar-categoria')
-  public async setCategoria(@Payload() categoria: ICategoria) {
-    return this.appService.setCategoria(categoria)
+  @MessagePattern('consultar-categorias')
+  public async getCategorias(@Ctx() ctx: RmqContext, @Payload() id: string) {
+    const channel = ctx.getChannelRef()
+    const originalMsg = ctx.getMessage()
+
+    try {
+      if (id) {
+        return this.appService.getCategoria(id)
+      }
+
+      return this.appService.getCategorias()
+    } finally {
+      await channel.ack(originalMsg)
+    }
   }
 
-  @MessagePattern('consultar-categorias')
-  public async getCategorias(@Payload() id: string) {
-    if (id) {
-      return this.appService.getCategoria(id)
-    }
+  @EventPattern('criar-categoria')
+  public async storeCategoria(
+    @Ctx() ctx: RmqContext,
+    @Payload() categoria: ICategoria,
+  ) {
+    const channel = ctx.getChannelRef()
+    const originalMsg = ctx.getMessage()
 
-    return this.appService.getCategorias()
+    try {
+      await this.appService.storeCategoria(categoria)
+
+      await channel.ack(originalMsg)
+    } catch (error) {
+      if (this.ackErrors.filter(ackError => error.message.includes(ackError))) {
+        await channel.ack(originalMsg)
+      }
+    }
+  }
+
+  @EventPattern('atualizar-categoria')
+  public async updateCategoria(@Ctx() ctx: RmqContext, @Payload() data: any) {
+    const channel = ctx.getChannelRef()
+    const originalMsg = ctx.getMessage()
+
+    try {
+      await this.appService.updateCategoria(data.id, data.categoria)
+
+      await channel.ack(originalMsg)
+    } catch (error) {
+      if (this.ackErrors.filter(ackError => error.message.includes(ackError))) {
+        await channel.ack(originalMsg)
+      }
+    }
   }
 }
