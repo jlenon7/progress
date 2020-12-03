3a123736e17209d25fc934c36c3061ad5f687b53
João Lenon <lenonsec7@gmail.com>
2020-12-02 21:27:44 -0300

    feat: Microservices NestJS e RabbitMQ

--- Udemy/Microservices em Node.js com NestJS e RabbitMQ/Gateway/src/proxyrmq/proxyrmq.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common'
-import { ClientProxySmartRanking } from './client-proxy'
-
-@Module({
-  providers: [ClientProxySmartRanking],
-  exports: [ClientProxySmartRanking],
-})
-export class ProxyRMQModule {}
