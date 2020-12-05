333e26ec269f51f3de8eedf9028fb5c6538bda33
João Lenon <lenonsec7@gmail.com>
2020-12-05 11:56:44 -0300

    feat: GraphQL Criando APIs Profissionais e Flexíveis

--- Udemy/GraphQL Criando APIs Profissionais e Flexíveis/projeto/index.js
+++ Udemy/GraphQL Criando APIs Profissionais e Flexíveis/projeto/index.js
@@ -1,29 +1,88 @@
 const { ApolloServer, gql } = require('apollo-server')
 
+const perfis = [
+    { id: 1, nome: 'comum' },
+    { id: 2, nome: 'administrador' }
+]
+
+const usuarios = [{
+        id: 1,
+        nome: 'João Silva',
+        email: 'jsilva@zemail.com',
+        idade: 29,
+        perfil_id: 1,
+    },
+    {
+        id: 2,
+        nome: 'Rafael Junior',
+        email: 'rafajun@wemail.com',
+        idade: 31,
+        perfil_id: 2,
+    },
+    {
+        id: 3,
+        nome: 'Daniela Smith',
+        email: 'danismi@uemail.com',
+        idade: 24,
+        perfil_id: 1,
+    },
+]
+
 const typeDefs = gql`
     scalar Date
+
+    type Produto {
+        nome: String!
+        preco: Float!
+        desconto: Float
+        precoComDesconto: Float
+    }
     
-    type User {
-        id: ID!
+    type Perfil {
+        id: Int
+        nome: String
+    }
+    
+    type Usuario {
+        id: Int!
         nome: String
         email: String
         idade: Int
         salario: Float
         vip: Boolean
+        perfil: Perfil
     }
     
     # Pontos de entrada da API
     type Query {
         ola: String
         horaAtual: Date
-        me: User
+        me: Usuario
+        produtoEmDestaque: Produto
+        numerosMegaSena: [Int!]!
+        usuarios: [Usuario]
+        usuario(id: Int): Usuario
+        perfis: [Perfil]
+        perfil(id: Int): Perfil
     }
 `
 
 const resolvers = {
-    User: {
-      salario(user) {
-
+    Produto: {
+        precoComDesconto(produto) {
+            if(produto.desconto) {
+                return produto.preco * (1 - produto.desconto)
+            } else {
+                return produto.preco
+            }
+        }
+    },
+    Usuario: {
+      salario(usuario) {
+        return usuario.salario_real
+      },
+      perfil(usuario) {
+          return perfis.find(p => p.id === usuario.perfil_id)
       }
     },
     Query: {
@@ -33,15 +92,38 @@ const resolvers = {
         horaAtual() {
             return new Date()
         },
-        me() {
+        me(obj) {
+            console.log(obj)
             return {
                 id: 1,
                 nome: 'João Lenon',
                 email: 'jlenon7@hotmail.com',
                 idade: 23,
-                salario: 6000.00,
+                salario_real: 6000.00,
                 vip: true,
             }
+        },
+        produtoEmDestaque() {
+            return {
+                nome: 'Notebook Gamer',
+                preco: 4890.89,
+                desconto: 0.5
+            }
+        },
+        numerosMegaSena() {
+            return Array(6).fill(0).map(() => parseInt(Math.random() * 60 + 1)).sort((a, b) => a - b)
+        },
+        usuarios() {
+            return usuarios
+        },
+        usuario(_, { id }) {
+            return usuarios.find(u => u.id === id)
+        },
+        perfis() {
+            return perfis
+        },
+        perfil(_, { id }) {
+            return perfis.find(p => p.id === id)
         }
     }
 }
