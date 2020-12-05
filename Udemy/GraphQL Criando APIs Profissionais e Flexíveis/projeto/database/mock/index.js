dc98f1593da6f80e6f3c57a895c4b8d6b7fd5c32
João Lenon <lenonsec7@gmail.com>
2020-12-05 16:02:23 -0300

    feat: GraphQL Criando APIs Profissionais e Flexíveis

---
+++ Udemy/GraphQL Criando APIs Profissionais e Flexíveis/projeto/database/mock/index.js
@@ -0,0 +1,32 @@
+const perfis = [
+  { id: 1, nome: 'comum' },
+  { id: 2, nome: 'administrador' }
+]
+
+const usuarios = [{
+      id: 1,
+      nome: 'João Silva',
+      email: 'jsilva@zemail.com',
+      idade: 29,
+      perfil_id: 1,
+      status: 'ATIVO'
+  },
+  {
+      id: 2,
+      nome: 'Rafael Junior',
+      email: 'rafajun@wemail.com',
+      idade: 31,
+      perfil_id: 2,
+      status: 'INATIVO'
+  },
+  {
+      id: 3,
+      nome: 'Daniela Smith',
+      email: 'danismi@uemail.com',
+      idade: 24,
+      perfil_id: 1,
+      status: 'BLOQUEADO'
+  },
+]
+
+module.exports = { usuarios, perfis }
