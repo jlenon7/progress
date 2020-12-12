3d360cc2c3667c67904972687a68714ea3d7b4f7
João Lenon <lenonsec7@gmail.com>
2020-12-12 16:26:37 -0300

    feat: New implementations

---
+++ app/Services/Api/UserService.ts
@@ -0,0 +1,31 @@
+import { BaseService } from 'App/Services'
+import { UserRepository } from 'App/Repositories/UserRepository'
+import { ApiRequestContract } from 'App/Contracts/ApiRequestContract'
+
+import NotFoundException from 'App/Exceptions/NotFoundException'
+
+export class UserService extends BaseService {
+  public async getAll(data?: ApiRequestContract) {
+    return new UserRepository().getAll(data)
+  }
+
+  public async getOne(id: string, data?: ApiRequestContract) {
+    const user = await new UserRepository().getOne(id, data)
+
+    if (!user) {
+      throw new NotFoundException()
+    }
+
+    return user
+  }
+
+  public async delete(id: string) {
+    const user = await new UserRepository().delete(id)
+
+    if (!user) {
+      throw new NotFoundException()
+    }
+
+    return user
+  }
+}
