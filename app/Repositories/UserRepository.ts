3a9f37a7e358a17bf99747fa170a37631a5f3898
João Lenon <lenonsec7@gmail.com>
2020-12-12 16:26:23 -0300

    feat: New implementations

--- app/Repositories/UserRepository.ts
+++ app/Repositories/UserRepository.ts
@@ -2,7 +2,5 @@ import User from 'App/Models/User'
 import { BaseRepository } from './BaseRepository'
 
 export class UserRepository extends BaseRepository {
-  constructor() {
-    super({ model: User })
-  }
+  protected Model = User
 }
