cb1ffde1f2a0d6bb29505f9764dc005c593f98c6
João Lenon <lenonsec7@gmail.com>
2020-12-14 00:04:13 -0300

    feat: ACL Implementation

--- app/Validators/Auth/ConfirmValidator.ts
+++ app/Validators/Auth/ConfirmValidator.ts
@@ -5,14 +5,11 @@ export class ConfirmValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    user_id: schema.string({}, [rules.exists({ table: 'users', column: 'id' })]),
     token: schema.string({}, [rules.exists({ table: 'user_tokens', column: 'token' })]),
   })
 
   public messages = {
-    'user_id.string': 'User ID should be a valid string',
     'token.string': 'Token should be a valid string',
-    'user_id.string.exists': 'User ID does not exists in Database',
     'token.string.exists': 'Token does not exists in Database',
   }
 }
