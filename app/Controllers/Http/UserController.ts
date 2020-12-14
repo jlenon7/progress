d89266675e9e01c5c56e44b2a50acee1ae59f436
João Lenon <lenonsec7@gmail.com>
2020-12-14 00:03:35 -0300

    feat: ACL Implementation

--- app/Controllers/Http/UserController.ts
+++ app/Controllers/Http/UserController.ts
@@ -3,12 +3,12 @@ import { ApiController } from 'App/Controllers/ApiController'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class UserController extends ApiController {
-  public async index({ request, response, auth }: HttpContextContract) {
+  public async index({ request, response, auth, pagination }: HttpContextContract) {
     const data = request.only(['where', 'orderBy', 'includes'])
 
-    const users = await new UserService().setGuard(auth).getAll(data)
+    const users = await new UserService().setGuard(auth).getAll(pagination, data)
 
-    return this.response(response).withCollection(users)
+    return this.response(response).withCollection(users.toJSON())
   }
 
   public async show({ request, response, params, auth }: HttpContextContract) {
@@ -22,6 +22,6 @@ export default class UserController extends ApiController {
   public async delete({ response, params, auth }: HttpContextContract) {
     const user = await new UserService().setGuard(auth).delete(params.id)
 
-    return this.response(response).withOne(user)
+    return this.response(response).withSoftDeleted(user.name)
   }
 }
