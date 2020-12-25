072145c92b8b5c4986ed08a4131beb1995334bd8
João Lenon <lenonsec7@gmail.com>
2020-12-25 00:21:06 -0300

    feat: Go Explorando a Linguagem do Google

---
+++ Udemy/Go Explorando a linguagem da Google/arrayslicemap/slice/slice.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	a1 := [3]int{1, 2, 3} // array
+	s1 := []int{1, 2, 3}  // slice
+
+	fmt.Println(a1, s1)
+	fmt.Println(reflect.TypeOf(a1), reflect.TypeOf(s1))
+
+	a2 := [5]int{1, 2, 3, 4, 5}
+	s2 := a2[1:3]
+	fmt.Println(a2, s2)
+
+	s3 := a2[:2]
+	fmt.Println(a2, s3)
+
+	s4 := s2[:1]
+	fmt.Println(s2, s4)
+}
