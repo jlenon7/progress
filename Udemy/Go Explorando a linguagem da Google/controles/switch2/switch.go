de5ba96275aebc20afb2635cc56564fab776d236
João Lenon <lenonsec7@gmail.com>
2020-12-24 21:19:21 -0300

    feat: Go Explorando a Linguagem do Google

---
+++ Udemy/Go Explorando a linguagem da Google/controles/switch2/switch.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Bom dia!")
+	case t.Hour() < 18:
+		fmt.Println("Boa tarde.")
+	default:
+		fmt.Println("Boa noite.")
+	}
+}
