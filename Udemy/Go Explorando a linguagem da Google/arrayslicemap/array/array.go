53440251d570383292af248604a8c7594be72e3a
João Lenon <lenonsec7@gmail.com>
2020-12-25 00:20:47 -0300

    feat: Go Explorando a Linguagem do Google

---
+++ Udemy/Go Explorando a linguagem da Google/arrayslicemap/array/array.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var notas [3]float64
+	fmt.Println(notas)
+
+	notas[0], notas[1], notas[2] = 7.8, 4.3, 9.1
+	fmt.Println(notas)
+
+	total := 0.0
+	for i := 0; i < len(notas); i++ {
+		total += notas[i]
+	}
+
+	media := total / float64(len(notas))
+	fmt.Printf("Média %.2f\n", media)
+}
