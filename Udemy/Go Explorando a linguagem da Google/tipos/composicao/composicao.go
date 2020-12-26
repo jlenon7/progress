489f84bc9c9b5fbcee2bff8096a4d2c13b4218c3
João Lenon <lenonsec7@gmail.com>
2020-12-26 14:43:05 -0300

    feat: Go Explorando a Linguagem do Google

---
+++ Udemy/Go Explorando a linguagem da Google/tipos/composicao/composicao.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type esportivo interface {
+	ligarTurbo()
+}
+
+type luxuoso interface {
+	fazerBaliza()
+}
+
+type esportivoLuxuoso interface {
+	esportivo
+	luxuoso
+}
+
+type bmw7 struct{}
+
+func (b bmw7) ligarTurbo() {
+	fmt.Println("Turbo...")
+}
+
+func (b bmw7) fazerBaliza() {
+	fmt.Println("Baliza...")
+}
+
+func main() {
+	var b esportivoLuxuoso = bmw7{}
+	b.ligarTurbo()
+	b.fazerBaliza()
+}
