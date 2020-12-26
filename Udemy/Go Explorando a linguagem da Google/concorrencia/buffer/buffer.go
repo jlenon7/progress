85b38a25922cbf37d6cef37655c6ddf5026e8ac4
João Lenon <lenonsec7@gmail.com>
2020-12-26 17:38:16 -0300

    feat: Go Explorando a Linguagem do Google

---
+++ Udemy/Go Explorando a linguagem da Google/concorrencia/buffer/buffer.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func rotina(ch chan int) {
+	ch <- 1
+	ch <- 2
+	ch <- 3
+	fmt.Println("Executou!")
+
+	ch <- 4
+	fmt.Println("Não sera executado, buffer lotado!")
+
+	ch <- 5
+	ch <- 6
+}
+
+func main() {
+	ch := make(chan int, 3)
+	go rotina(ch)
+
+	fmt.Println(<-ch)
+}
