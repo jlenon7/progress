a45ba4789ff11e86aa5620c54d72767e80062769
João Lenon <lenonsec7@gmail.com>
2020-12-26 17:38:55 -0300

    feat: Go Explorando a Linguagem do Google

---
+++ Udemy/Go Explorando a linguagem da Google/concorrencia/goroutine/goroutine.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func fale(pessoa, texto string, qtde int) {
+	for i := 0; i < qtde; i++ {
+		time.Sleep(time.Second)
+		fmt.Printf("%s: %s (iteração %d)\n", pessoa, texto, i+1)
+	}
+}
+
+func main() {
+	go fale("Maria", "Ei...", 10)
+	fale("João", "Opa...", 5)
+}
