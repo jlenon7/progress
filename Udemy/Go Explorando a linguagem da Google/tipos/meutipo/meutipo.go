16ffcfaaa7d24ec62a7407ff9c0b0e5814b20f74
João Lenon <lenonsec7@gmail.com>
2020-12-26 14:43:21 -0300

    feat: Go Explorando a Linguagem do Google

---
+++ Udemy/Go Explorando a linguagem da Google/tipos/meutipo/meutipo.go
@@ -0,0 +1,25 @@
+package main
+
+type nota float64
+
+func (n nota) entre(inicio, fim float64) bool {
+	return float64(n) >= inicio && float64(n) <= fim
+}
+
+func notaParaConceito(n nota) string {
+	if n.entre(9.0, 10.0) {
+		return "A"
+	} else if n.entre(7.0, 8.99) {
+		return "B"
+	} else if n.entre(5.0, 7.99) {
+		return "C"
+	} else if n.entre(3.0, 4.99) {
+		return "D"
+	} else {
+		return "E"
+	}
+}
+
+func main() {
+	println(notaParaConceito(10.0))
+}
