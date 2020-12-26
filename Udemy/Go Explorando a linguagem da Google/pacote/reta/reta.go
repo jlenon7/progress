926652126c77769a36484ffe4d8009bb04be8756
João Lenon <lenonsec7@gmail.com>
2020-12-26 17:38:41 -0300

    feat: Go Explorando a Linguagem do Google

---
+++ Udemy/Go Explorando a linguagem da Google/pacote/reta/reta.go
@@ -0,0 +1,23 @@
+package main
+
+import "math"
+
+// Ponto representa uma coordenada no plano cartesiano
+type Ponto struct {
+	x float64
+	y float64
+}
+
+func catetos(a, b Ponto) (cx, cy float64) {
+	cx = math.Abs(b.x - a.x)
+	cy = math.Abs(b.y - a.y)
+
+	return
+}
+
+// Distancia é responsável por calcular a distância linear entre dois pontos
+func Distancia(a, b Ponto) float64 {
+	cx, cy := catetos(a, b)
+
+	return math.Sqrt(math.Pow(cx, 2) + math.Pow(cy, 2))
+}
