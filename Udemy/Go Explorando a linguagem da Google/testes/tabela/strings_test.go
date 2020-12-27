9bcb718a5d263d32bd4784cf5d134dd345effe86
João Lenon <lenonsec7@gmail.com>
2020-12-27 11:53:39 -0300

    feat: Go Explorando a Linguagem do Google

---
+++ Udemy/Go Explorando a linguagem da Google/testes/tabela/strings_test.go
@@ -0,0 +1,30 @@
+package strings
+
+import (
+	"strings"
+	"testing"
+)
+
+const msgIndex = "%s (parte: %s) - Índices: esperado (%d) <> encontrado (%d)."
+
+func TestIndex(t *testing.T) {
+	testes := []struct {
+		texto    string
+		parte    string
+		esperado int
+	}{
+		{"Cod3r é show", "Cod3r", 0},
+		{"", "", 0},
+		{"Opa", "opa", -1},
+		{"leonardo", "o", 2},
+	}
+
+	for _, teste := range testes {
+		t.Logf("Massa: %v", teste)
+		atual := strings.Index(teste.texto, teste.parte)
+
+		if atual != teste.esperado {
+			t.Errorf(msgIndex, teste.texto, teste.parte, teste.esperado, atual)
+		}
+	}
+}
