e1e9487321b86ea22f4d7cf02bb19a20bbf01353
João Lenon <lenonsec7@gmail.com>
2020-12-27 15:06:37 -0300

    feat: Go Explorando a Linguagem do Google

--- Udemy/Go Explorando a linguagem da Google/testes/tabela/strings_test.go
+++ Udemy/Go Explorando a linguagem da Google/testes/tabela/strings_test.go
@@ -8,6 +8,7 @@ import (
 const msgIndex = "%s (parte: %s) - Índices: esperado (%d) <> encontrado (%d)."
 
 func TestIndex(t *testing.T) {
+	t.Parallel()
 	testes := []struct {
 		texto    string
 		parte    string
