f01a0812e54ad7cfd95d05525c10c34eb85cc6db
João Lenon <lenonsec7@gmail.com>
2020-12-30 17:50:49 -0300

    fix: Merge Conflicts

--- app/Models/Hooks/OrderItemHook.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const OrderItemHook = (exports = module.exports = {})
-const Product = use('App/Models/Product')
-
-OrderItemHook.updateSubtotal = async model => {
-  let product = await Product.find(model.product_id)
-  model.subtotal = model.quantity * product.price
-}
